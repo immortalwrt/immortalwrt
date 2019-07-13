694fdcfbd72de40ddb843a43527c79436985ad48
CN_SZTL <cnsztl@gmail.com>
2019-07-14 04:28:27 +0800

    luci-app-flowoffload:fix denpention && luci-app-unblockmusic: add more function

--- package/lean/luci-app-unblockmusic/luasrc/model/cbi/unblockmusic.lua
+++ package/lean/luci-app-unblockmusic/luasrc/model/cbi/unblockmusic.lua
@@ -1,6 +1,6 @@
 
 mp = Map("unblockmusic", translate("解锁网易云灰色歌曲"))
-mp.description = translate("原理：采用 [网易云旧链/QQ/虾米/百度/酷狗/酷我/咕咪/JOOX]等音源 替换网易云变灰歌曲链接<br />具体使用方法可查看github：<br />https://github.com/maxlicheng/luci-app-unblockmusic")
+mp.description = translate("原理：采用 [网易云旧链/QQ/虾米/百度/酷狗/酷我/咕咪/JOOX]等音源，替换网易云变灰歌曲链接<br/>具体使用方法可查看GitHub：https://github.com/maxlicheng/luci-app-unblockmusic")
 
 mp:section(SimpleSection).template  = "unblockmusic/unblockmusic_status"
 
@@ -12,18 +12,43 @@ enabled = s:option(Flag, "enabled", translate("启用解锁"))
 enabled.default = 0
 enabled.rmempty = false
 
+enabled = s:option(Flag, "strict_mode", translate("启用严格模式"))
+enabled.default = 0
+enabled.rmempty = false
+
 speedtype = s:option(ListValue, "musicapptype", translate("音源选择"))
 speedtype:value("default", translate("默认"))
-speedtype:value("NeteaseCloud", translate("网易云音乐"))
-speedtype:value("QQ", translate("QQ音乐"))
+speedtype:value("netease", translate("网易云音乐"))
+speedtype:value("qq", translate("QQ音乐"))
 speedtype:value("xiami", translate("虾米音乐"))
 speedtype:value("baidu", translate("百度音乐"))
 speedtype:value("kugou", translate("酷狗音乐"))
-speedtype:value("koowo", translate("酷我音乐"))
+speedtype:value("kuwo", translate("酷我音乐"))
 speedtype:value("migu", translate("咕咪音乐"))
 speedtype:value("joox", translate("JOOX音乐"))
+speedtype:value("all", translate("所有平台"))
 
 account = s:option(Value, "port", translate("端口号"))
 account.datatype = "string"
+
+enabled = s:option(Flag, "set_netease_server_ip", translate("自定义网易云服务器IP"))
+enabled.description = translate("自定义网易云服务器IP地址，如使用Hosts方式则必选")
+enabled.default = 0
+enabled.rmempty = false
+
+account = s:option(Value, "netease_server_ip", translate("网易云服务器IP"))
+account.description = translate("通过 ping music.163.com 即可获得，仅限填写一个")
+account.default = "59.111.181.38"
+account.datatype = "string"
+account:depends("set_netease_server_ip", 1)
+
+enabled = s:option(Flag, "enable_proxy", translate("使用代理服务器"))
+enabled.description = translate("具体格式请参考：https://github.com/nondanee/UnblockNeteaseMusic")
+enabled.default = 0
+enabled.rmempty = false
+
+account = s:option(Value, "proxy_server_ip", translate("代理服务器IP"))
+account.datatype = "string"
+account:depends("enable_proxy", 1)
 
 return mp
