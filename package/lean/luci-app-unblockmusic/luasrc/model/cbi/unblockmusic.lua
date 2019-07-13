fe4faa3d07bbda8e870b53f0a5b9b5a903c14177
CN_SZTL <cnsztl@gmail.com>
2019-07-14 04:46:24 +0800

    modified:   package/lean/luci-app-unblockmusic/luasrc/model/cbi/unblockmusic.lua

--- package/lean/luci-app-unblockmusic/luasrc/model/cbi/unblockmusic.lua
+++ package/lean/luci-app-unblockmusic/luasrc/model/cbi/unblockmusic.lua
@@ -13,7 +13,7 @@ enabled.default = 0
 enabled.rmempty = false
 
 enabled = s:option(Flag, "strict_mode", translate("启用严格模式"))
-enabled.description = translate("若将服务部署到公网，强烈建议使用严格模式，此模式下仅放行网易云音乐所属域名的请求")
+enabled.description = translate("若将服务部署到公网，则强烈建议使用严格模式，此模式下仅放行网易云音乐所属域名的请求")
 enabled.default = 0
 enabled.rmempty = false
 
@@ -33,22 +33,23 @@ account = s:option(Value, "port", translate("端口号"))
 account.datatype = "string"
 
 enabled = s:option(Flag, "set_netease_server_ip", translate("自定义网易云服务器IP"))
-enabled.description = translate("自定义网易云服务器IP地址，如使用Hosts方式则必选")
+enabled.description = translate("自定义网易云服务器IP地址；如使用Hosts方式则必选，否则将会导致连接死循环")
 enabled.default = 0
 enabled.rmempty = false
 
 account = s:option(Value, "netease_server_ip", translate("网易云服务器IP"))
-account.description = translate("通过 ping music.163.com 即可获得，仅限填写一个")
+account.description = translate("通过 ping music.163.com 即可获得IP地址，仅限填写一个")
 account.default = "59.111.181.38"
 account.datatype = "string"
 account:depends("set_netease_server_ip", 1)
 
 enabled = s:option(Flag, "enable_proxy", translate("使用代理服务器"))
-enabled.description = translate("具体格式请参考：https://github.com/nondanee/UnblockNeteaseMusic")
+enabled.description = translate("如您的OpenWRT系统部署在海外，则此选项必选，否则可能无法正常使用")
 enabled.default = 0
 enabled.rmempty = false
 
 account = s:option(Value, "proxy_server_ip", translate("代理服务器IP"))
+account.description = translate("具体格式请参考：https://github.com/nondanee/UnblockNeteaseMusic")
 account.datatype = "string"
 account:depends("enable_proxy", 1)
 
