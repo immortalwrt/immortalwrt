0b3e78494490db399733c4a4c5a25cf119659f54
CN_SZTL <cnsztl@gmail.com>
2019-08-03 00:56:25 +0800

    luci-app-flowoffload: fix running status check

--- package/lean/luci-app-flowoffload/luasrc/controller/flowoffload.lua
+++ package/lean/luci-app-flowoffload/luasrc/controller/flowoffload.lua
@@ -17,7 +17,7 @@ local function is_running()
 end
 
 local function is_bbr()
-	return luci.sys.call("[ `cat /proc/sys/net/ipv4/tcp_congestion_control 2>/dev/null` = `uci get flowoffload.@flow[0].bbr 2>/dev/null` ] 2>/dev/null") == 0
+	return luci.sys.call("[ `cat /proc/sys/net/ipv4/tcp_congestion_control 2>/dev/null` = bbr ] 2>/dev/null") == 0
 end
 
 local function is_fullcone()
