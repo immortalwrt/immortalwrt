fd5cb05691f24ffb7aed957ef983632a81d50fd0
CN_SZTL <cnsztl@gmail.com>
2019-08-17 22:05:03 +0800

    luci-app-baidupcs-web: fix checking running status

--- package/cnsztl/luci-app-baidupcs-web/files/luci/model/cbi/baidupcs-web.lua
+++ package/cnsztl/luci-app-baidupcs-web/files/luci/model/cbi/baidupcs-web.lua
@@ -7,7 +7,7 @@ local uci = require "luci.model.uci".cursor()
 
 local m, s
 
-local running=(luci.sys.call("pidof baidupcs-web > /dev/null") == 0)
+local running=(luci.sys.call("pidof BaiduPCS-Web > /dev/null") == 0)
 
 local button = ""
 local state_msg = ""
