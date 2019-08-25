588ed8081495557813507833c9e0b13b7611409b
CN_SZTL <cnsztl@gmail.com>
2019-08-25 14:39:05 +0800

    luci-app-sfe: fix AdGuardHome settings

--- package/lean/luci-app-sfe/root/usr/share/dnscache/dnscache-while.sh
+++ package/lean/luci-app-sfe/root/usr/share/dnscache/dnscache-while.sh
@@ -2,6 +2,7 @@
 
 sleeptime=30
 logfile="/var/log/dnscache.file"
+adg_logfile="/etc/AdGuardHome/data/querylog.json"
 dns_enable=$(uci get sfe.config.dns 2>/dev/null)
 dnscache_enable=$(uci get sfe.config.dnscache_enable 2>/dev/null)
 
@@ -10,6 +11,13 @@ logrow=$(grep -c "" ${logfile})
 if [ $logrow -ge 500 ];then
     cat /dev/null > ${logfile}
     echo "$curtime Log条数超限，清空处理！" >> ${logfile}
+fi
+if [ $dnscache_enable = "3" ];then
+    adg_logrow=$(grep -c "" ${adg_logfile})
+    if [ $adg_logrow -ge 500 ];then
+        cat /dev/null > ${adg_logfile}
+        echo "$curtime Log条数超限，清空处理！" >> ${adg_logfile}
+    fi
 fi
 
 }
@@ -24,14 +32,11 @@ if [ $dns_enable -eq 1 ]; then
 			AdGuardHome -c /etc/AdGuardHome/AdGuardHome.yaml -w /etc/AdGuardHome >/dev/null 2>&1 &
 			echo "$curtime 重启服务！" >> ${logfile}
 		fi
-		if ! pidof dnscache>/dev/null; then
-			dnscache -f /var/run/dnscache/dnscache.conf -d
-		fi
 	else
 		if ! pidof dnscache>/dev/null; then
 			if [ $dnscache_enable = "1" ];  then
 			/usr/sbin/dnscache -c /var/etc/dnscache.conf -d
-			elif [ $dnscache_enable = "2" ] || [ $dnscache_enable = "3" ];  then
+			elif [ $dnscache_enable = "2" ];  then
 			dnscache -f /var/run/dnscache/dnscache.conf -d
 			fi
 			echo "$curtime 重启服务！" >> ${logfile}
@@ -42,4 +47,3 @@ fi
 sleep ${sleeptime}
 continue
 done
-
