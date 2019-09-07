358a2cb79323314ab0e2f526eb2555e05040e346
CN_SZTL <cnsztl@gmail.com>
2019-09-07 15:50:19 +0800

    luci-app-qbittorrent: move to nas

--- package/lean/luci-app-qbittorrent/src/controller/qbittorrent.lua
+++ package/lean/luci-app-qbittorrent/src/controller/qbittorrent.lua
@@ -1,4 +1,4 @@
 module("luci.controller.qbittorrent",package.seeall)
 function index()
-	entry({"admin","services","qbittorrent"},cbi("qbittorrent"),_("qbittorrent"))
+	entry({"admin","nas","qbittorrent"},cbi("qbittorrent"),_("qbittorrent"))
 end
