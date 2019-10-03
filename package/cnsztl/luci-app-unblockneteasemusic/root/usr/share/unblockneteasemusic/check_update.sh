f86a0073d9c1b0a10de36cf828f2cd736dc1d9f7
CN_SZTL <cnsztl@gmail.com>
2019-10-03 15:52:38 +0800

    luci-app-unblockneteasemusic: meaningless update

--- package/cnsztl/luci-app-unblockneteasemusic/root/usr/share/unblockneteasemusic/check_update.sh
+++ package/cnsztl/luci-app-unblockneteasemusic/root/usr/share/unblockneteasemusic/check_update.sh
@@ -3,7 +3,7 @@
 # https://github.com/project-openwrt
 
 commit_info="$(curl -s https://github.com/nondanee/UnblockNeteaseMusic/commits/master)"
-latest_ver="$(echo -e "${commit_info}" |tr '\n' ' ' | grep -Eo 'commit\/[0-9a-z]+' |sed -n 1p |sed 's#commit/##g')"
+latest_ver="$(echo -e "${commit_info}" |tr -d '\n' | grep -Eo 'commit\/[0-9a-z]+' |sed -n 1p |sed 's#commit/##g')"
 latest_ver_mini="$(echo -e "${commit_info}" |tr -d '\n' | grep -Eo 'BtnGroup-item">      [0-9a-z]+' |sed -n 1p |sed 's#BtnGroup-item">      ##g')"
 
 [ -z "${commit_info}" ] && echo "无法检测最新版本，请稍后重试" && exit 1
