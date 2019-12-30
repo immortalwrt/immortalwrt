27b1610628a8ec040501d998369bdb90037c1f27
CN_SZTL <cnsztl@gmail.com>
2019-12-30 17:30:38 +0800

    luci-app-unblockneteasemusic-mini: sync upstream source

--- package/ctcgfw/luci-app-unblockneteasemusic-mini/luasrc/model/cbi/unblockneteasemusic-mini.lua
+++ package/ctcgfw/luci-app-unblockneteasemusic-mini/luasrc/model/cbi/unblockneteasemusic-mini.lua
@@ -1,7 +1,7 @@
 -- Created By [CTCGFW]Project-OpenWrt
 -- https://github.com/project-openwrt
 
-mp = Map("unblockneteasemusic-mini", translate("解除网易云音乐播放限制"))
+mp = Map("unblockneteasemusic-mini", translate("解除网易云音乐播放限制 (Mini)"))
 mp.description = translate("原理：采用 [QQ/虾米/百度/酷狗/酷我/咕咪/JOOX] 等音源，替换网易云音乐 无版权/收费 歌曲链接<br/>由 [CTCGFW]Project-OpenWrt 提供服务器支持，来源: https://github.com/project-openwrt/luci-app-unblockneteasemusic-mini")
 
 mp:section(SimpleSection).template = "unblockneteasemusic-mini/unblockneteasemusic_mini_status"
