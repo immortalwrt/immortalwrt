7f61731c2fa892eaead640321f393836273ff8f2
CN_SZTL <cnsztl@gmail.com>
2019-12-30 20:46:00 +0800

    luci-app-ssr-plus-Jo: Merge upstream source

--- package/ctcgfw/luci-app-ssr-plus-Jo/luasrc/model/cbi/shadowsocksr/servers.lua
+++ package/ctcgfw/luci-app-ssr-plus-Jo/luasrc/model/cbi/shadowsocksr/servers.lua
@@ -1,10 +1,10 @@
 -- Licensed to the public under the GNU General Public License v3.
 local d = require "luci.dispatcher"
-
+local fs = require "nixio.fs"
+local sys = require "luci.sys"
 local uci = require "luci.model.uci".cursor()
 local m, s, o
 local shadowsocksr = "shadowsocksr"
-
 local uci = luci.model.uci.cursor()
 local server_count = 0
 uci:foreach("shadowsocksr", "servers", function(s)
@@ -12,97 +12,6 @@ uci:foreach("shadowsocksr", "servers", function(s)
 end)
 
 m = Map(shadowsocksr)
-local server_table = {}
-local encrypt_methods = {
-	"none",
-	"table",
-	"rc4",
-	"rc4-md5-6",
-	"rc4-md5",
-	"aes-128-cfb",
-	"aes-192-cfb",
-	"aes-256-cfb",
-	"aes-128-ctr",
-	"aes-192-ctr",
-	"aes-256-ctr",	
-	"bf-cfb",
-	"camellia-128-cfb",
-	"camellia-192-cfb",
-	"camellia-256-cfb",
-	"cast5-cfb",
-	"des-cfb",
-	"idea-cfb",
-	"rc2-cfb",
-	"seed-cfb",
-	"salsa20",
-	"chacha20",
-	"chacha20-ietf",
-}
-local encrypt_methods_ss = {
-	-- aead
-	"aes-128-gcm",
-	"aes-192-gcm",
-	"aes-256-gcm",
-	"chacha20-ietf-poly1305",
-	"xchacha20-ietf-poly1305",
-	-- stream
-	"table",
-	"rc4",
-	"rc4-md5",
-	"aes-128-cfb",
-	"aes-192-cfb",
-	"aes-256-cfb",
-	"aes-128-ctr",
-	"aes-192-ctr",
-	"aes-256-ctr",
-	"bf-cfb",
-	"camellia-128-cfb",
-	"camellia-192-cfb",
-	"camellia-256-cfb",
-	"salsa20",
-	"chacha20",
-	"chacha20-ietf",
-}
-local protocol = {
-	"origin",
-	"verify_deflate",		
-	"auth_sha1_v4",
-	"auth_aes128_sha1",
-	"auth_aes128_md5",
-	"auth_chain_a",
-	"auth_chain_b",
-	"auth_chain_c",
-	"auth_chain_d",
-	"auth_chain_e",
-	"auth_chain_f",
-}
-
-obfs = {
-	"plain",
-	"http_simple",
-	"http_post",
-	"random_head",	
-	"tls1.2_ticket_auth",
-}
-local obfs_opts = {
-	"none",
-	"http",
-	"tls",
-}
-
-local securitys = {
-    "auto",
-    "none",
-    "aes-128-gcm",
-    "chacha20-poly1305"
-}
-uci:foreach(shadowsocksr, "servers", function(s)
-	if s.alias then
-		server_table[s[".name"]] = s.alias
-	elseif s.server and s.server_port then
-		server_table[s[".name"]] = "%s:%s" %{s.server, s.server_port}
-	end
-end)
 m:section(SimpleSection).template  = "shadowsocksr/status"
 -- [[ Servers Manage ]]--
 s = m:section(TypedSection, "servers")
