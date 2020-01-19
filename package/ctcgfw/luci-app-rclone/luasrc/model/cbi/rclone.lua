b1241c92a00b6f56c45f6c49955877d7819dfb2f
CN_SZTL <cnsztl@gmail.com>
2020-01-19 11:45:32 +0800

    luci-app-rclone: sync with upstream source

--- package/ctcgfw/luci-app-rclone/luasrc/model/cbi/rclone.lua
+++ package/ctcgfw/luci-app-rclone/luasrc/model/cbi/rclone.lua
@@ -12,10 +12,23 @@ local running = (luci.sys.call('pidof rclone > /dev/null') == 0)
 local state_msg = ''
 local trport = uci:get('rclone', 'config', 'port')
 
+local triptype = uci.get('rclone', 'config', 'addr_type')
+local trip = ''
+
+if triptype == 'local' then
+    trip = uci.get('network', 'loopback', 'ipaddr')
+elseif triptype == 'lan' then
+    trip = uci.get('network', 'lan', 'ipaddr')
+else
+    trip = '[ip]'
+end
+
 if running then
     state_msg = '<b><font color="green">' .. translate('rclone running') .. '</font></b>'
+    address_msg = translate('rclone address') .. ' : http://' .. trip .. ':' .. trport .. '<br/> <br/>'
 else
     state_msg = '<b><font color="red">' .. translate('rclone not run') .. '</font></b>'
+    address_msg = ''
 end
 
 m =
@@ -23,15 +36,12 @@ m =
     'rclone',
     translate('Rclone'),
     translate('Rclone ("rsync for cloud storage") is a command line program to sync root/usr/bin and directories to and from different cloud storage providers.') ..
-        ' <br/> <br/> ' ..
-            translate('rclone state') ..
-                ' : ' ..
-                    state_msg ..
-                        '<br/> <br/>' ..
-                            translate('Installed Web Interface') ..
-                                '&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<input type="button" class="cbi-button" style="margin: 0 5px;" value=" ' ..
-                                    translate('Webui React') ..
-                                        " \" onclick=\"window.open('http://'+window.location.hostname+'/rclone-webui-react')\"/> <br/><br/>"
+        ' <br/> <br/> ' .. translate('rclone state') .. ' : ' .. state_msg .. '<br/> <br/>'
+        .. address_msg ..
+        translate('Installed Web Interface') ..
+        '&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<input type="button" class="cbi-button" style="margin: 0 5px;" value=" ' ..
+        translate('Webui React') ..
+        " \" onclick=\"window.open('http://'+window.location.hostname+'/rclone-webui-react')\"/> <br/><br/>"
 )
 
 s = m:section(TypedSection, 'global', translate('global'))
@@ -48,7 +58,7 @@ s.anonymous = true
 o = s:option(ListValue, 'addr_type', translate('listen address'))
 o:value('local', translate('localhost address'))
 o:value('lan', translate('local network address'))
-o:value('wan', translate('all address'))
+o:value('all', translate('all address'))
 o.default = 'lan'
 o.rmempty = false
 
@@ -59,8 +69,8 @@ o.datatype = 'port'
 o.rmempty = false
 
 o = s:option(Value, 'config_path', translate('rclone configuration file path'))
-o.placeholder = '/etc/rclone/rclone.conf'
-o.default = '/etc/rclone/rclone.conf'
+o.placeholder = '/var/etc/rclone/rclone.conf'
+o.default = '/var/etc/rclone/rclone.conf'
 o.rmempty = false
 
 o = s:option(Value, 'username', translate('username'))
