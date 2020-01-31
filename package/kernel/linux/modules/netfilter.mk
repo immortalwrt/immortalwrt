e5cfc29192006e7151b1be2ea561a572b34fedfa
CN_SZTL <cnsztl@gmail.com>
2020-02-01 05:05:44 +0800

    cgroup: update description

--- package/kernel/linux/modules/netfilter.mk
+++ package/kernel/linux/modules/netfilter.mk
@@ -460,11 +460,17 @@ $(eval $(call KernelPackage,ipt-nat))
 
 define KernelPackage/ipt-cgroup
   SUBMENU:=$(NF_MENU)
-  TITLE:=cgroup
+  TITLE:=cgroup netfilter module
   KCONFIG:=$(KCONFIG_IPT_CGROUP)
   FILES:=$(LINUX_DIR)/net/netfilter/*cgroup*.$(LINUX_KMOD_SUFFIX)
   AUTOLOAD:=$(call AutoLoad,$(notdir $(IPT_CGROUP-m)))
   DEPENDS:= kmod-ipt-core
+endef
+
+define KernelPackage/ipt-cgroup
+ Kernel support for cgroup netfilter module
+ Include:
+ - cgroup
 endef
 
 $(eval $(call KernelPackage,ipt-cgroup))
