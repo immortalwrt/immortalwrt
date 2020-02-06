47060323400bf277edb25b30a96623b33c847e6e
CN_SZTL <cnsztl@gmail.com>
2020-02-07 04:26:50 +0800

    luci-app-qbittorrent: almost rewrite

--- package/lean/luci-app-qbittorrent/luasrc/model/cbi/qbittorrent.lua
+++ package/lean/luci-app-qbittorrent/luasrc/model/cbi/qbittorrent.lua
@@ -5,28 +5,32 @@ local a=(luci.sys.call("pidof qbittorrent-nox > /dev/null")==0)
 
 local t=""
 if a then
-t="<br /><br /><input class=\"cbi-button cbi-button-apply\" type=\"submit\" value=\" "..translate("Open Web Interface").." \" onclick=\"window.open('http://'+window.location.hostname+':"..o.."')\"/>"
+	t="<br/><br/><input type=\"button\" value=\" " .. translate("Open Web Interface") .. " \" onclick=\"window.open('http://'+window.location.hostname+':" .. o .. "')\"/>"
 end
 
-m = Map("qbittorrent", translate("qBittorrent"), translate("qBittorrent is a cross-platform free and open-source BitTorrent client")..t)
+m = Map("qbittorrent", translate("qBittorrent"), translate("qBittorrent is a cross-platform free and open-source BitTorrent client.").."<br/>"..translate("Default login username: admin, password: adminadmin.")..t)
 
 m:section(SimpleSection).template="qbittorrent/qbittorrent_status"
 
-s_basic = m:section(TypedSection, "basic", translate("Basic Settings"))
-s_basic.anonymous = true
+s = m:section(TypedSection, "qbittorrent")
+s.anonymous=true
 
-enable = s_basic:option(Flag, "enable", translate("Enable"))
-profile_dir = s_basic:option(Value,"profile_dir",translate("profile_dir"),translate("Store configuration files in the Path"))
-profile_dir.default = "/root"
+enable = s:option(Flag, "enable", translate("Enable"))
+enable.rmempty = false
 
-s_download = m:section(TypedSection, "Preferences", translate("Download Settings"))
-s_download.anonymous = true
-download_dir = s_download:option(Value,"download_dir",translate("download_dir"),translate("Store download files in the Path"))
-download_dir.default = "/root/download"
-
-s_webui = m:section(TypedSection, "Preferences", translate("WEBUI Settings"))
-s_webui.anonymous = true
-port = s_webui:option(Value,"port",translate("port"),translate("WEBUI listening port"))
+port = s:option(Value,"port",translate("Port"),translate("WebUI listening port"))
 port.default = "8080"
+port.placeholder = "8080"
+port.rmempty = false
+
+profile_dir = s:option(Value,"profile_dir",translate("Profile Dir"),translate("Store configuration files in the Path"))
+profile_dir.default = "/root"
+profile_dir.placeholder = "/root"
+profile_dir.rmempty = false
+
+download_dir = s:option(Value,"download_dir",translate("Download Dir"),translate("Store download files in the Path"))
+download_dir.default = "/mnt/download"
+download_dir.placeholder = "/mnt/download"
+download_dir.rmempty = false
 
 return m
