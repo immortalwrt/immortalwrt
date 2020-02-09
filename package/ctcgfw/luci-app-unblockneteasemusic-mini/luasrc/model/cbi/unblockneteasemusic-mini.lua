69de12d605ce8da3a014a18ceaedfd1fe2b9ea5b
CN_SZTL <cnsztl@gmail.com>
2020-02-09 16:24:32 +0800

    luci-app-unblockneteasemusic-mini: bump to 1.2

--- package/ctcgfw/luci-app-unblockneteasemusic-mini/luasrc/model/cbi/unblockneteasemusic-mini.lua
+++ package/ctcgfw/luci-app-unblockneteasemusic-mini/luasrc/model/cbi/unblockneteasemusic-mini.lua
@@ -15,7 +15,14 @@ enable.description = translate("启用本插件以解除网易云音乐播放限
 enable.default = 0
 enable.rmempty = false
 
+select_server = s:option(ListValue, "select_server", translate("服务端类型"))
+select_server:value("tencent_shanghai_nodejs", translate("腾讯云上海 Node.js 版本（稳定）"))
+select_server:value("anhui_telecom_golang", translate("安徽电信 Golang 版本（高速率）"))
+select_server.description = translate("Node.js版使用QQ、酷我、咪咕音源，Golang版使用酷狗、酷我音源")
+select_server.default = "tencent_shanghai_nodejs"
+select_server.rmempty = false
+
 download_certificate=s:option(DummyValue,"opennewwindow",translate("<input type=\"button\" class=\"cbi-button cbi-button-apply\" value=\"下载CA根证书\" onclick=\"window.open('https://raw.githubusercontent.com/nondanee/UnblockNeteaseMusic/master/ca.crt')\" />"))
-download_certificate.description = translate("Linux/iOS/MacOSX设备在信任根证书后方可正常使用解锁功能。")
+download_certificate.description = translate("Linux/iOS/iPad/MacOSX设备在信任根证书后方可正常使用解锁功能。")
 
 return mp
