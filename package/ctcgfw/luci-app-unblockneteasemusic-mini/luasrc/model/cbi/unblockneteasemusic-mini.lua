93cfea5a50f2cfedbb3b4ea40a46882f6cdf169c
CN_SZTL <cnsztl@gmail.com>
2020-02-18 13:57:42 +0800

    luci-app-unblockneteasemusic-mini: bump to v1.2-4

--- package/ctcgfw/luci-app-unblockneteasemusic-mini/luasrc/model/cbi/unblockneteasemusic-mini.lua
+++ package/ctcgfw/luci-app-unblockneteasemusic-mini/luasrc/model/cbi/unblockneteasemusic-mini.lua
@@ -2,7 +2,7 @@
 -- https://github.com/project-openwrt
 
 mp = Map("unblockneteasemusic-mini", translate("解除网易云音乐播放限制 (Mini)"))
-mp.description = translate("原理：采用 [QQ/酷狗/酷我/咕咪] 等音源，替换网易云音乐 无版权/收费 歌曲链接<br/>由 [CTCGFW]Project-OpenWrt 提供服务器支持，来源: https://github.com/project-openwrt/luci-app-unblockneteasemusic-mini")
+mp.description = translate("原理：采用 [QQ/酷狗/酷我/咕咪] 等音源，替换网易云音乐 无版权/收费 歌曲链接<br/>由 [CTCGFW]Project-OpenWrt & @hyird 提供服务器支持<br/>详细说明参见：https://github.com/project-openwrt/luci-app-unblockneteasemusic-mini")
 
 mp:section(SimpleSection).template = "unblockneteasemusic-mini/unblockneteasemusic_mini_status"
 
@@ -17,6 +17,7 @@ enable.rmempty = false
 
 select_server = s:option(ListValue, "select_server", translate("服务端类型"))
 select_server:value("tencent_shanghai_nodejs", translate("腾讯云上海 Node.js 版本（高音质）"))
+select_server:value("tencent_beijing_nodejs", translate("腾讯云北京 Node.js 版本（高音质）"))
 select_server:value("tencent_shanghai_golang", translate("腾讯云上海 Golang 版本（低音质）"))
 select_server.description = translate("Node.js版使用QQ、酷我、咪咕音源，Golang版使用酷狗、酷我、咪咕音源")
 select_server.default = "tencent_shanghai_nodejs"
