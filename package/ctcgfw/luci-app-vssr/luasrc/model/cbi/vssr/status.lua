4215ee2407fc32bf1dde1f4ac8611ddce4842840
CN_SZTL <cnsztl@gmail.com>
2020-02-26 15:51:14 +0800

    luci-app-vssr: sync with upstream source

--- package/ctcgfw/luci-app-vssr/luasrc/model/cbi/vssr/status.lua
+++ package/ctcgfw/luci-app-vssr/luasrc/model/cbi/vssr/status.lua
@@ -1,7 +1,7 @@
 -- Copyright (C) 2017 yushi studio <ywb94@qq.com>
 -- Licensed to the public under the GNU General Public License v3.
 
-local IPK_Version="20200222.1.12"
+local IPK_Version="20200225.1.13"
 local m, s, o
 local redir_run=0
 local reudp_run=0
