1fb7b958aa259edacd434eda1d89e562b8b8c43f
CN_SZTL <cnsztl@gmail.com>
2020-03-04 12:19:58 +0800

    luci-app-ssr-plus-Jo: sync with upstream source

--- package/ctcgfw/luci-app-ssr-plus-Jo/luasrc/model/cbi/shadowsocksr/status.lua
+++ package/ctcgfw/luci-app-ssr-plus-Jo/luasrc/model/cbi/shadowsocksr/status.lua
@@ -1,7 +1,7 @@
 -- Copyright (C) 2017 yushi studio <ywb94@qq.com>
 -- Licensed to the public under the GNU General Public License v3.
 
-local IPK_Version="20200301.150"
+local IPK_Version="20200304.151"
 local m, s, o
 local redir_run=0
 local reudp_run=0
@@ -96,6 +96,88 @@ if nixio.fs.access("/etc/china_ssr.txt") then
  ip_count = sys.exec("cat /etc/china_ssr.txt | wc -l")
 end
 
+function processlist()
+	local data = {}
+	local netf = {}
+	local k
+	local ps = luci.util.execi("/bin/busybox top -bn1 | egrep -v dnsmasq")
+	local nets = luci.util.execi("netstat -netupl | egrep -v dnsmasq | awk '{print $1,$4,_,$6,$7}'")
+
+	if not ps or not nets then
+		return
+	end
+
+	for line in nets do
+-- tcp        0      0 127.0.0.1:1234          0.0.0.0:*               LISTEN      5103/v2ray
+-- udp        0      0 127.0.0.1:1234          0.0.0.0:*                           5147/v2ray
+--		local proto, ip, port, nid = line:match("([^%s]+) +.* +([^ ]*):(%d+) +.* +(%d+)\/.*")
+		local proto, ip, port, nid = line:match("([^%s]+) (.*):(%d+)[^%d]+(%d+)\/.*")
+		local idx = tonumber(nid)
+		if idx and ip then
+			local newstr = "://" .. ip .. ":" .. port
+			local isnew = (netf[idx] and netf[idx]['listen']) and netf[idx]['listen']:match(proto .. newstr)  or false
+			netf[idx] = {
+				['listen'] = ((netf[idx] and netf[idx]['listen']) and (not isnew) and (netf[idx]['listen'] .. "\n" .. proto) or proto) .. newstr,
+			}
+		end
+	end
+-- 5103     1 root     S     661m 543%   0% /usr/bin/v2ray/v2ray -config /var/etc/shadowsocksr.json
+	for line in ps do
+		local pid, ppid, user, stat, vsz, mem, cpu, cmd = line:match(
+			"^ *(%d+) +(%d+) +(%S.-%S) +([RSDZTW][W ][<N ]) +(%d+.?) +(%d+%%) +(%d+%%) +(.+)"
+		)
+		if cmd then
+		local idx = tonumber(pid)
+		local bin, param, cfg = cmd:match("^.*\/([^ ]*) *([^ ]*) *\/var\/etc\/([^ ]*).*")
+		if idx and cfg then
+			local listen = "NONE"
+			if netf[idx] and netf[idx]['listen'] then
+				listen = netf[idx]['listen']
+			end
+			data[idx] = {
+				['PID']     = pid,
+				['COMMAND'] = bin,
+				['LISTEN'] = listen,
+				['CONFIG']   = cfg,
+				['%MEM']    = mem,
+				['%CPU']    = cpu,
+			}
+		end
+		end
+	end
+
+	return data
+end
+
+function printstat(status, form, name)
+	local tabs = {
+		["Global Client"] = "shadowsocksr.json",
+		["Game Mode UDP Relay"] = "shadowsocksr_u.json",
+		["PDNSD"] = "pdnsd.conf",
+		["DNS Forward"] = "shadowsocksr_d.json",
+		["SOCKS5 Proxy"] = "shadowsocksr_s.json",
+		["Global SSR Server"] = "shadowsocksr_0.json",
+		
+	}
+	local stat = translate("Unknown")
+	local sname = stat
+	if tabs[name] and status then
+	stat = translate("Not Running")
+	for idx, cfg in pairs(status) do
+		if status[idx]['CONFIG'] and status[idx]['CONFIG'] == tabs[name] then
+			stat = font_blue .. bold_on .. translate("Running") .. bold_off .. " > " .. status[idx]['COMMAND'] .. " -c " .. status[idx]['CONFIG'] .. font_off
+			sname = translate(status[idx]['COMMAND'])
+			break
+		end
+	end
+	end
+	local section = form:field(DummyValue,name,translate(name) .. ": " .. sname)
+	section.rawhtml  = true
+	section.value = stat
+	return section
+end
+
+procs=processlist()
 
 
 
@@ -187,7 +269,12 @@ m = SimpleForm("Version")
 m.reset = false
 m.submit = false
 
-
+t = m:section(Table, procs, translate("Running Details: ") .. "(/var/etc)")
+t:option(DummyValue, "PID", translate("PID"))
+t:option(DummyValue, "COMMAND", translate("CMD"))
+t:option(DummyValue, "LISTEN", translate("LISTEN"))
+t:option(DummyValue, "%CPU", translate("CPU"))
+t:option(DummyValue, "%MEM", translate("MEM"))
 
 s=m:field(DummyValue,"redir_run",translate("Global Client")) 
 s.rawhtml  = true
