2776aa59283aea687c2957f674250b05e5fffc90
CN_SZTL <cnsztl@project-openwrt.eu.org>
2020-03-07 20:31:59 +0800

    luci-app-unblockneteasemusic-go: bump to 1.6

--- package/lean/luci-app-unblockneteasemusic-go/root/usr/share/UnblockNeteaseMusic/log_check.sh
+++ package/lean/luci-app-unblockneteasemusic-go/root/usr/share/UnblockNeteaseMusic/log_check.sh
@@ -1,8 +1,12 @@
 #!/bin/bash
-log_max_size="16" #使用KB计算
+log_max_size="128" #使用KB计算
 log_file="/tmp/unblockneteasemusic.log"
 
-(( log_size = "$(ls -l "${log_file}" | awk -F ' ' '{print $5}')" / "1024" ))
-(( "${log_size}" >= "${log_max_size}" )) && echo "" > "${log_file}"
-
-[ "*$(uci get unblockneteasemusic.@unblockneteasemusic[0].daemon_enable)*" == "*1*" ] && { [ -z "$(ps |grep "UnblockNeteaseMusic" |grep -v "grep")" ] && /etc/init.d/unblockneteasemusic restart; }
+((log_size = "$(ls -l "${log_file}" | awk -F ' ' '{print $5}')" / "1024"))
+(("${log_size}" >= "${log_max_size}")) && echo "" >"${log_file}"
+if [ "$(uci get unblockneteasemusic.@unblockneteasemusic[0].daemon_enable)" == "1" ]; then
+    if [ -z "$(ps | grep "UnblockNeteaseMusic" | grep -v "grep")" ]; then
+        echo "$(date -R) 尝试重启应用..." >>"${log_file}"
+        /etc/init.d/unblockneteasemusic restart
+    fi
+fi
