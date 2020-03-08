a26f89e7341ef6ecc44386373fd86e9056ecdeff
CN_SZTL <cnsztl@project-openwrt.eu.org>
2020-03-08 16:21:32 +0800

    luci-app-filebrowser: improve button

--- package/ctcgfw/luci-app-filebrowser/luasrc/controller/filebrowser.lua
+++ package/ctcgfw/luci-app-filebrowser/luasrc/controller/filebrowser.lua
@@ -7,4 +7,12 @@ function index()
 	local page
 	page = entry({"admin", "nas", "filebrowser"}, cbi("filebrowser"), _("文件管理器"), 100)
 	page.dependent = true
+	entry({"admin","nas","filebrowser","status"},call("act_status")).leaf=true
+end
+
+function act_status()
+	local e={}
+	e.running=luci.sys.call("pgrep filebrowser >/dev/null")==0
+	luci.http.prepare_content("application/json")
+	luci.http.write_json(e)
 end
