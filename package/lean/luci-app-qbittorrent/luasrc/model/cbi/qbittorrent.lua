2e4a7d8319851978c4a6de7d8eac40c702922249
CN_SZTL <cnsztl@project-openwrt.eu.org>
2020-03-08 16:11:16 +0800

    luci-app-qbittorrent: improve button

--- package/lean/luci-app-qbittorrent/luasrc/model/cbi/qbittorrent.lua
+++ package/lean/luci-app-qbittorrent/luasrc/model/cbi/qbittorrent.lua
@@ -1,13 +1,3 @@
-local e=require"luci.model.uci".cursor()
-local o=e:get_first("qbittorrent","qbittorrent","port") or 8080
-
-local a=(luci.sys.call("pidof qbittorrent-nox > /dev/null")==0)
-
-local t=""
-if a then
-	t="<br/><br/><input type=\"button\" value=\" " .. translate("Open Web Interface") .. " \" onclick=\"window.open('http://'+window.location.hostname+':" .. o .. "')\"/>"
-end
-
 m = Map("qbittorrent", translate("qBittorrent"), translate("qBittorrent is a cross-platform free and open-source BitTorrent client.").."<br/>"..translate("Default login username: admin, password: adminadmin.")..t)
 
 m:section(SimpleSection).template="qbittorrent/qbittorrent_status"
