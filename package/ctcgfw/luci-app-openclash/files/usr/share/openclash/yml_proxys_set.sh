2da6de56538c0c5c0a2a743f41efddd88f7dd8a2
CN_SZTL <cnsztl@project-openwrt.eu.org>
2020-03-21 01:25:37 +0800

    OpenClash: sync with upstream source

--- package/ctcgfw/luci-app-openclash/files/usr/share/openclash/yml_proxys_set.sh
+++ package/ctcgfw/luci-app-openclash/files/usr/share/openclash/yml_proxys_set.sh
@@ -103,7 +103,7 @@ set_alpn()
       return
    fi
 cat >> "$SERVER_FILE" <<-EOF
-    - "$1"
+    - $1
 EOF
 }
 
@@ -183,7 +183,11 @@ yml_servers_set()
       fi
    fi
    
-   if [ ! -z "$udp" ] && [ "$obfs" = "none" ]; then
+   if [ ! -z "$udp" ] && [ "$obfs" = "none" ] && [ "$type" = "ss" ]; then
+      udp=", udp: $udp"
+   fi
+   
+   if [ ! -z "$udp" ] && [ "$type" != "trojan" ] && [ "$type" != "ss" ]; then
       udp=", udp: $udp"
    fi
    
@@ -219,7 +223,7 @@ yml_servers_set()
       fi
    fi
    
-   if [ ! -z "$skip_cert_verify" ] && [ "$type" != "ss" ]; then
+   if [ ! -z "$skip_cert_verify" ] && [ "$type" != "ss" ] && [ "$type" != "trojan" ]; then
       skip_cert_verify=", skip-cert-verify: $skip_cert_verify"
    elif [ ! -z "$skip_cert_verify" ]; then
       skip_cert_verify="skip-cert-verify: $skip_cert_verify"
@@ -278,10 +282,14 @@ EOF
    fi
    
    if [ "$type" = "vmess" ]; then
-      echo "- { name: \"$name\", type: $type, server: $server, port: $port, uuid: $uuid, alterId: $alterId, cipher: $securitys$skip_cert_verify$obfs_vmess$path$custom$tls }" >>$SERVER_FILE
+      echo "- { name: \"$name\", type: $type, server: $server, port: $port, uuid: $uuid, alterId: $alterId, cipher: $securitys$udp$skip_cert_verify$obfs_vmess$path$custom$tls }" >>$SERVER_FILE
    fi
    
-   if [ "$type" = "socks5" ] || [ "$type" = "http" ]; then
+   if [ "$type" = "socks5" ]; then
+      echo "- { name: \"$name\", type: $type, server: $server, port: $port, username: $auth_name, password: $auth_pass$udp$skip_cert_verify$tls }" >>$SERVER_FILE
+   fi
+   
+   if [ "$type" = "http" ]; then
       echo "- { name: \"$name\", type: $type, server: $server, port: $port, username: $auth_name, password: $auth_pass$skip_cert_verify$tls }" >>$SERVER_FILE
    fi
    
@@ -311,7 +319,7 @@ EOF
    fi
    if [ ! -z "$skip_cert_verify" ]; then
 cat >> "$SERVER_FILE" <<-EOF
-    $skip_cert_verify
+  $skip_cert_verify
 EOF
    fi
    fi
