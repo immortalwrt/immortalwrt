fcc82e37ba3f8c1d14d6b310ed05345e02f7adbe
CN_SZTL <cnsztl@project-openwrt.eu.org>
2020-03-21 22:30:02 +0800

    autocore: show cpubench score

--- package/lean/coremark/coremark.sh
+++ package/lean/coremark/coremark.sh
@@ -3,7 +3,7 @@
 /bin/coremark > /tmp/coremark.log
 
 cat /tmp/coremark.log | grep "CoreMark 1.0" | cut -d  "/"  -f 1 > /etc/bench.log
-sed -i 's/CoreMark 1.0/(CpuMark/g'  /etc/bench.log
+sed -i 's/CoreMark 1.0/ (CpuMark/g'  /etc/bench.log
 echo " Scores)" >> /etc/bench.log
 
 if [ -f "/etc/bench.log" ]; then
