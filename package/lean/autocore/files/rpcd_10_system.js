18c06251454ebeb1fc7ffd3de2d4c33d768b0e3b
CN_SZTL <cnsztl@project-openwrt.eu.org>
2020-03-21 20:50:52 +0800

    autocore: drop useless arch info

--- package/lean/autocore/files/rpcd_10_system.js
+++ package/lean/autocore/files/rpcd_10_system.js
@@ -67,7 +67,6 @@ return L.Class.extend({
 			_('Hostname'),         boardinfo.hostname,
 			_('Model'),            boardinfo.model,
 			_('CPU Info'),         cpuinfo.cpuinfo,
-			_('Architecture'),     boardinfo.system,
 			_('Firmware Version'), (L.isObject(boardinfo.release) ? boardinfo.release.description + ' / ' : '') + (luciversion || ''),
 			_('Kernel Version'),   boardinfo.kernel,
 			_('Local Time'),       datestr,
