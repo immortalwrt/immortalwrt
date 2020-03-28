5ab26148dc9e8e76ea152fee76c3b0db0096947e
CN_SZTL <cnsztl@project-openwrt.eu.org>
2020-03-28 15:50:23 +0800

    luci-app-vssr: sync with upstream source

--- package/ctcgfw/luci-app-vssr/luasrc/model/cbi/vssr/advanced.lua
+++ package/ctcgfw/luci-app-vssr/luasrc/model/cbi/vssr/advanced.lua
@@ -28,6 +28,16 @@ o.rmempty = false
 o = s:option(Flag, "enable_switch", translate("Enable Auto Switch"))
 o.rmempty = false
 
+-- [[ adblock ]]--
+o = s:option(Flag, "adblock", translate("Enable adblock"))
+o.rmempty = false
+
+o = s:option(Value, "adblock_url", translate("adblock_url"))
+o:value("https://gitee.com/privacy-protection-tools/anti-ad/raw/master/anti-ad-for-dnsmasq.conf", translate("anti-AD"))
+o.default = "https://gitee.com/privacy-protection-tools/anti-ad/raw/master/anti-ad-for-dnsmasq.conf"
+o:depends("adblock", "1")
+o.description = translate("Support AdGuardHome and DNSMASQ format list")
+
 o = s:option(Value, "switch_time", translate("Switch check cycly(second)"))
 o.datatype = "uinteger"
 o:depends("enable_switch", "1")
@@ -69,16 +79,6 @@ o.datatype = "port"
 o.default = 1081
 o.rmempty = false
 end
--- [[ adblock ]]--
-s = m:section(TypedSection, "global", translate("adblock settings"))
-s.anonymous = true
-
-o = s:option(Flag, "adblock", translate("Enable adblock"))
-o.rmempty = false
-
-o = s:option(Value, "adblock_url", translate("adblock_url"))
-o.default = "https://gitee.com/privacy-protection-tools/anti-ad/raw/master/anti-ad-for-dnsmasq.conf"
-o.description = translate("Support AdGuardHome and DNSMASQ format list")
 
 -- [[ chnroute ]]
 s = m:section(TypedSection, "global", translate("Chnroute Setting"))
