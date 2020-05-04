9b2ec85c65b6c1e878655caac5582d1368a2102a
CN_SZTL <cnsztl@project-openwrt.eu.org>
2020-05-04 16:32:41 +0800

    luci-app-ssrserver-python: fully convert to python3

--- package/lean/luci-app-ssrserver-python/root/usr/share/ssr/shadowsocks/shell.py
+++ package/lean/luci-app-ssrserver-python/root/usr/share/ssr/shadowsocks/shell.py
@@ -1,4 +1,4 @@
-#!/usr/bin/python
+#!/usr/bin/python3
 # -*- coding: utf-8 -*-
 #
 # Copyright 2015 clowwindy
--- package/lean/luci-app-ssrserver-python/root/usr/share/ssr/tests/jenkins.sh
+++ package/lean/luci-app-ssrserver-python/root/usr/share/ssr/tests/jenkins.sh
@@ -24,47 +24,47 @@ function run_test {
     return 0
 }
 
-python --version
+python3 --version
 coverage erase
 mkdir tmp
 run_test pep8 --ignore=E402 .
 run_test pyflakes .
 run_test coverage run tests/nose_plugin.py -v
-run_test python setup.py sdist
+run_test python3 setup.py sdist
 run_test tests/test_daemon.sh
-run_test python tests/test.py --with-coverage -c tests/aes.json
-run_test python tests/test.py --with-coverage -c tests/aes-ctr.json
-run_test python tests/test.py --with-coverage -c tests/aes-cfb1.json
-run_test python tests/test.py --with-coverage -c tests/aes-cfb8.json
-run_test python tests/test.py --with-coverage -c tests/rc4-md5.json
-run_test python tests/test.py --with-coverage -c tests/salsa20.json
-run_test python tests/test.py --with-coverage -c tests/chacha20.json
-run_test python tests/test.py --with-coverage -c tests/table.json
-run_test python tests/test.py --with-coverage -c tests/server-multi-ports.json
-run_test python tests/test.py --with-coverage -s tests/aes.json -c tests/client-multi-server-ip.json
-run_test python tests/test.py --with-coverage -s tests/server-multi-passwd.json -c tests/server-multi-passwd-client-side.json
-run_test python tests/test.py --with-coverage -c tests/workers.json
-run_test python tests/test.py --with-coverage -s tests/ipv6.json -c tests/ipv6-client-side.json
-run_test python tests/test.py --with-coverage -b "-m rc4-md5 -k testrc4 -s 127.0.0.1 -p 8388 -q" -a "-m rc4-md5 -k testrc4 -s 127.0.0.1 -p 8388 -l 1081 -vv"
-run_test python tests/test.py --with-coverage -b "-m aes-256-cfb -k testrc4 -s 127.0.0.1 -p 8388 --workers 1" -a "-m aes-256-cfb -k testrc4 -s 127.0.0.1 -p 8388 -l 1081 -t 30 -qq -b 127.0.0.1"
-run_test python tests/test.py --with-coverage --should-fail --url="http://127.0.0.1/" -b "-m aes-256-cfb -k testrc4 -s 127.0.0.1 -p 8388 --forbidden-ip=127.0.0.1,::1,8.8.8.8" -a "-m aes-256-cfb -k testrc4 -s 127.0.0.1 -p 8388 -l 1081 -t 30 -b 127.0.0.1"
+run_test python3 tests/test.py --with-coverage -c tests/aes.json
+run_test python3 tests/test.py --with-coverage -c tests/aes-ctr.json
+run_test python3 tests/test.py --with-coverage -c tests/aes-cfb1.json
+run_test python3 tests/test.py --with-coverage -c tests/aes-cfb8.json
+run_test python3 tests/test.py --with-coverage -c tests/rc4-md5.json
+run_test python3 tests/test.py --with-coverage -c tests/salsa20.json
+run_test python3 tests/test.py --with-coverage -c tests/chacha20.json
+run_test python3 tests/test.py --with-coverage -c tests/table.json
+run_test python3 tests/test.py --with-coverage -c tests/server-multi-ports.json
+run_test python3 tests/test.py --with-coverage -s tests/aes.json -c tests/client-multi-server-ip.json
+run_test python3 tests/test.py --with-coverage -s tests/server-multi-passwd.json -c tests/server-multi-passwd-client-side.json
+run_test python3 tests/test.py --with-coverage -c tests/workers.json
+run_test python3 tests/test.py --with-coverage -s tests/ipv6.json -c tests/ipv6-client-side.json
+run_test python3 tests/test.py --with-coverage -b "-m rc4-md5 -k testrc4 -s 127.0.0.1 -p 8388 -q" -a "-m rc4-md5 -k testrc4 -s 127.0.0.1 -p 8388 -l 1081 -vv"
+run_test python3 tests/test.py --with-coverage -b "-m aes-256-cfb -k testrc4 -s 127.0.0.1 -p 8388 --workers 1" -a "-m aes-256-cfb -k testrc4 -s 127.0.0.1 -p 8388 -l 1081 -t 30 -qq -b 127.0.0.1"
+run_test python3 tests/test.py --with-coverage --should-fail --url="http://127.0.0.1/" -b "-m aes-256-cfb -k testrc4 -s 127.0.0.1 -p 8388 --forbidden-ip=127.0.0.1,::1,8.8.8.8" -a "-m aes-256-cfb -k testrc4 -s 127.0.0.1 -p 8388 -l 1081 -t 30 -b 127.0.0.1"
 
 # test if DNS works
-run_test python tests/test.py --with-coverage -c tests/aes.json --url="https://clients1.google.com/generate_204"
+run_test python3 tests/test.py --with-coverage -c tests/aes.json --url="https://clients1.google.com/generate_204"
 
 # test localhost is in the forbidden list by default
-run_test python tests/test.py --with-coverage --should-fail --tcp-only --url="http://127.0.0.1/" -b "-m aes-256-cfb -k testrc4 -s 127.0.0.1 -p 8388" -a "-m aes-256-cfb -k testrc4 -s 127.0.0.1 -p 8388 -l 1081 -t 30 -b 127.0.0.1"
+run_test python3 tests/test.py --with-coverage --should-fail --tcp-only --url="http://127.0.0.1/" -b "-m aes-256-cfb -k testrc4 -s 127.0.0.1 -p 8388" -a "-m aes-256-cfb -k testrc4 -s 127.0.0.1 -p 8388 -l 1081 -t 30 -b 127.0.0.1"
 
 # test localhost is available when forbidden list is empty
-run_test python tests/test.py --with-coverage --tcp-only --url="http://127.0.0.1/" -b "-m aes-256-cfb -k testrc4 -s 127.0.0.1 -p 8388 --forbidden-ip=" -a "-m aes-256-cfb -k testrc4 -s 127.0.0.1 -p 8388 -l 1081 -t 30 -b 127.0.0.1"
+run_test python3 tests/test.py --with-coverage --tcp-only --url="http://127.0.0.1/" -b "-m aes-256-cfb -k testrc4 -s 127.0.0.1 -p 8388 --forbidden-ip=" -a "-m aes-256-cfb -k testrc4 -s 127.0.0.1 -p 8388 -l 1081 -t 30 -b 127.0.0.1"
 
 if [ -f /proc/sys/net/ipv4/tcp_fastopen ] ; then
     if [ 3 -eq `cat /proc/sys/net/ipv4/tcp_fastopen` ] ; then
         # we have to run it twice:
         # the first time there's no syn cookie
         # the second time there is syn cookie
-        run_test python tests/test.py --with-coverage -c tests/fastopen.json
-        run_test python tests/test.py --with-coverage -c tests/fastopen.json
+        run_test python3 tests/test.py --with-coverage -c tests/fastopen.json
+        run_test python3 tests/test.py --with-coverage -c tests/fastopen.json
     fi
 fi
 
