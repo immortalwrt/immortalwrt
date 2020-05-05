83a2987c4722a3b65994a7edb4f10a063392e7fe
Adrian Schmutzler <freifunk@adrianschmutzler.de>
2020-05-05 13:06:26 +0200

    sunxi: tidy up cases in base-files
    This removes unnecessary quotes in cases and applies alphabetic
    order to members.
    
    Signed-off-by: Adrian Schmutzler <freifunk@adrianschmutzler.de>

--- target/linux/sunxi/base-files/lib/preinit/03_b53_hack.sh
+++ target/linux/sunxi/base-files/lib/preinit/03_b53_hack.sh
@@ -5,7 +5,7 @@ do_b53_hack() {
 
 	# hack: enable switch on Lamobo R1 and reset counters
 	case $(board_name) in
-	"lamobo,lamobo-r1")
+	lamobo,lamobo-r1)
 		ip link set eth0 up
 		sleep 1
 		swconfig dev switch0 set reset 1
