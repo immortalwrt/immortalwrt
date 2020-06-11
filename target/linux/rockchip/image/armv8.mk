1544c2246ed015945d9f1044a6f491a9034a7bc8
Jayantajit Gogoi <jayanta.gogoi525@gmail.com>
2020-06-11 18:26:48 +0000

    rockchip: add support for rk3328 radxa rock pi e
    Signed-off-by: Jayantajit Gogoi <jayanta.gogoi525@gmail.com>

--- target/linux/rockchip/image/armv8.mk
+++ target/linux/rockchip/image/armv8.mk
@@ -4,11 +4,21 @@
 # This is free software, licensed under the GNU General Public License v2.
 # See /LICENSE for more information.
 #
+define Device/radxa_rock-pi-e
+  DEVICE_VENDOR := Radxa
+  DEVICE_MODEL := Rock Pi E
+  SOC := rk3328
+  UBOOT_DEVICE_NAME := rock-pi-e-rk3328
+  IMAGE/sysupgrade.img.gz := boot-common | boot-script | sdcard-img | gzip | append-metadata
+  DEVICE_PACKAGES += kmod-phy-realtek
+endef
+TARGET_DEVICES += radxa_rock-pi-e
+
 define Device/pine64_rockpro64
   DEVICE_VENDOR := Pine64
   DEVICE_MODEL := RockPro64
   SOC := rk3399
   UBOOT_DEVICE_NAME := rockpro64-rk3399
-  IMAGE/sysupgrade.img.gz := boot-common | boot-script | pine64-img | gzip | append-metadata
+  IMAGE/sysupgrade.img.gz := boot-common | boot-script | sdcard-img | gzip | append-metadata
 endef
 TARGET_DEVICES += pine64_rockpro64
