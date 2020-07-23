55fe4a0c83b64d72d57edecce8c9a29b8ac019b2
CN_SZTL <cnsztl@project-openwrt.eu.org>
2020-07-24 03:23:00 +0800

    target/rockchip: add missing dependency

--- target/linux/rockchip/armv8/target.mk
+++ target/linux/rockchip/armv8/target.mk
@@ -2,6 +2,8 @@ ARCH:=aarch64
 SUBTARGET:=armv8
 BOARDNAME:=RK33xx boards (64 bit)
 
+DEFAULT_PACKAGES += ethtool
+
 define Target/Description
 	Build firmware image for Rockchip RK33xx devices.
 	This firmware features a 64 bit kernel.
