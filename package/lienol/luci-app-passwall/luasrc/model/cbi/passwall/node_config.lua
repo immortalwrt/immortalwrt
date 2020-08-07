276ce1aedc5a9c49caa83bf96a8f0f1600d2310d
CN_SZTL <cnsztl@project-openwrt.eu.org>
2020-08-07 18:58:07 +0800

    luci-app-passwall: bump to 3.9-31

--- package/lienol/luci-app-passwall/luasrc/model/cbi/passwall/node_config.lua
+++ package/lienol/luci-app-passwall/luasrc/model/cbi/passwall/node_config.lua
@@ -1,19 +1,7 @@
 local d = require "luci.dispatcher"
-local ipkg = require("luci.model.ipkg")
 local uci = require"luci.model.uci".cursor()
 local api = require "luci.model.cbi.passwall.api.api"
-
 local appname = "passwall"
-
-local function get_customed_path(e)
-    return api.uci_get_type("global_app", e .. "_file")
-end
-
-local function is_finded(e)
-    return luci.sys.exec("find /usr/*bin %s -iname %s -type f" % {get_customed_path(e), e}) ~= "" and true or false
-end
-
-local function is_installed(e) return ipkg.installed(e) end
 
 local ss_encrypt_method_list = {
     "rc4-md5", "aes-128-cfb", "aes-192-cfb", "aes-256-cfb", "aes-128-ctr",
@@ -82,25 +70,25 @@ remarks.default = translate("Remarks")
 remarks.rmempty = false
 
 type = s:option(ListValue, "type", translate("Type"))
-if is_installed("ipt2socks") or is_finded("ipt2socks") then
+if api.is_finded("ipt2socks") then
     type:value("Socks", translate("Socks"))
 end
-if is_finded("ss-redir") then
+if api.is_finded("ss-redir") then
     type:value("SS", translate("Shadowsocks"))
 end
-if is_finded("ssr-redir") then
+if api.is_finded("ssr-redir") then
     type:value("SSR", translate("ShadowsocksR"))
 end
-if is_installed("v2ray") or is_finded("v2ray") then
+if api.is_finded("v2ray") then
     type:value("V2ray", translate("V2ray"))
 end
-if is_installed("brook") or is_finded("brook") then
+if api.is_finded("brook") then
     type:value("Brook", translate("Brook"))
 end
-if is_installed("trojan-plus") or is_finded("trojan-plus") then
-    type:value("Trojan", translate("Trojan-Plus"))
+if api.is_finded("trojan-plus") or api.is_finded("trojan") then
+    type:value("Trojan-Plus", translate("Trojan-Plus"))
 end
-if is_installed("trojan-go") or is_finded("trojan-go") then
+if api.is_finded("trojan-go") then
     type:value("Trojan-Go", translate("Trojan-Go"))
 end
 
@@ -166,7 +154,7 @@ address:depends({ type = "V2ray", protocol = "http" })
 address:depends({ type = "V2ray", protocol = "socks" })
 address:depends({ type = "V2ray", protocol = "shadowsocks" })
 address:depends("type", "Brook")
-address:depends("type", "Trojan")
+address:depends("type", "Trojan-Plus")
 address:depends("type", "Trojan-Go")
 
 --[[
@@ -180,7 +168,7 @@ use_ipv6:depends({ type = "V2ray", protocol = "http" })
 use_ipv6:depends({ type = "V2ray", protocol = "socks" })
 use_ipv6:depends({ type = "V2ray", protocol = "shadowsocks" })
 use_ipv6:depends("type", "Brook")
-use_ipv6:depends("type", "Trojan")
+use_ipv6:depends("type", "Trojan-Plus")
 use_ipv6:depends("type", "Trojan-Go")
 --]]
 
@@ -195,7 +183,7 @@ port:depends({ type = "V2ray", protocol = "http" })
 port:depends({ type = "V2ray", protocol = "socks" })
 port:depends({ type = "V2ray", protocol = "shadowsocks" })
 port:depends("type", "Brook")
-port:depends("type", "Trojan")
+port:depends("type", "Trojan-Plus")
 port:depends("type", "Trojan-Go")
 
 username = s:option(Value, "username", translate("Username"))
@@ -209,7 +197,7 @@ password:depends("type", "Socks")
 password:depends("type", "SS")
 password:depends("type", "SSR")
 password:depends("type", "Brook")
-password:depends("type", "Trojan")
+password:depends("type", "Trojan-Plus")
 password:depends("type", "Trojan-Go")
 password:depends("protocol", "http")
 password:depends("protocol", "socks")
@@ -260,13 +248,13 @@ tcp_fast_open:value("false")
 tcp_fast_open:value("true")
 tcp_fast_open:depends("type", "SS")
 tcp_fast_open:depends("type", "SSR")
-tcp_fast_open:depends("type", "Trojan")
+tcp_fast_open:depends("type", "Trojan-Plus")
 tcp_fast_open:depends("type", "Trojan-Go")
 
 ss_plugin = s:option(ListValue, "ss_plugin", translate("plugin"))
 ss_plugin:value("none", translate("none"))
-if is_finded("v2ray-plugin") then ss_plugin:value("v2ray-plugin") end
-if is_finded("obfs-local") then ss_plugin:value("obfs-local") end
+if api.is_finded("v2ray-plugin") then ss_plugin:value("v2ray-plugin") end
+if api.is_finded("obfs-local") then ss_plugin:value("obfs-local") end
 ss_plugin:depends("type", "SS")
 
 ss_plugin_opts = s:option(Value, "ss_plugin_opts", translate("opts"))
@@ -310,7 +298,7 @@ stream_security.default = "tls"
 stream_security:depends("protocol", "vmess")
 stream_security:depends("protocol", "socks")
 stream_security:depends("protocol", "shadowsocks")
-stream_security:depends("type", "Trojan")
+stream_security:depends("type", "Trojan-Plus")
 stream_security:depends("type", "Trojan-Go")
 stream_security.validate = function(self, value)
     if value == "none" and type:formvalue(arg[1]) == "Trojan" then
