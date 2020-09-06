08b71c12e687d46c5de2b63dfea367ce9bd34c52
CN_SZTL <cnsztl@project-openwrt.eu.org>
2020-09-06 12:21:30 +0800

    sunxi: drop support for nanopi duo2/r1/r1s

--- target/linux/sunxi/image/cortex-a53.mk
+++ target/linux/sunxi/image/cortex-a53.mk
@@ -38,19 +38,6 @@ define Device/friendlyarm_nanopi-neo2
 endef
 TARGET_DEVICES += friendlyarm_nanopi-neo2
 
-define Device/friendlyarm_nanopi-r1s
-  DEVICE_VENDOR := FriendlyARM
-  DEVICE_MODEL:=Nanopi R1S (H5)
-  DEVICE_PACKAGES:=kmod-rtc-sunxi \
-    kmod-leds-gpio kmod-ledtrig-heartbeat \
-    kmod-brcmfmac brcmfmac-firmware-43430-sdio wpad-basic \
-    kmod-usb-net kmod-usb-net-rtl8152
-  SUPPORTED_DEVICES:=nanopi-r1s
-  $(Device/sun50i-h5)
-  DEFAULT := n
-endef
-TARGET_DEVICES += friendlyarm_nanopi-r1s
-
 define Device/olimex_a64-olinuxino
   DEVICE_VENDOR := Olimex
   DEVICE_MODEL := A64-Olinuxino
