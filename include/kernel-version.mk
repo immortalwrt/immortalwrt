89005d1fddbb836075fee570cfef26e3388f1055
John Audia <graysky@archlinux.us>
2020-09-26 13:30:16 -0400

    kernel: bump 5.4 to 5.4.68
    All modifications made by update_kernel.sh
    
    Build system: x86_64
    Build-tested: ipq806x, ath79/generic, bcm72xx/bcm2711
    Run-tested: ipq806x (R7800)
    
    No dmesg regressions, everything functional
    
    Signed-off-by: John Audia <graysky@archlinux.us>

--- include/kernel-version.mk
+++ include/kernel-version.mk
@@ -8,11 +8,11 @@ endif
 
 LINUX_VERSION-4.14 = .198
 LINUX_VERSION-4.19 = .145
-LINUX_VERSION-5.4 = .67
+LINUX_VERSION-5.4 = .68
 
 LINUX_KERNEL_HASH-4.14.198 = 1efaba581860a36666c3ae5001a9f291d3d6929d9d5e78800ebd6242858cb503
 LINUX_KERNEL_HASH-4.19.145 = 17bc834ff8f15aee45b7ca7c8b9d43b6b03111a43eb7de75c808a04f4b3ff6b7
-LINUX_KERNEL_HASH-5.4.67 = c175bd9c5d54c7120365d51cf235102e14d5a9a1baddd6296819839240dccaa4
+LINUX_KERNEL_HASH-5.4.68 = 0e93876c5ae8dc0c55cbe631971a46ab02b90cf7461fed3085703a5e4e3cd6dd
 
 remove_uri_prefix=$(subst git://,,$(subst http://,,$(subst https://,,$(1))))
 sanitize_uri=$(call qstrip,$(subst @,_,$(subst :,_,$(subst .,_,$(subst -,_,$(subst /,_,$(1)))))))
