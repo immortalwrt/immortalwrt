eb64d4a34a4c230411755fb2cc84d3aaaba90359
John Audia <graysky@archlinux.us>
2020-10-01 09:22:00 -0400

    kernel: bump 5.4 to 5.4.69
    Seemingly unneeded based on new upstream code so manually deleted:
     platform/820-usb-0007-usb-dwc3-gadget-increase-timeout-value-for-send-ep-c.patch
    
    Manually merged:
     generic-hack/251-sound_kconfig.patch
    
    All other modifications made by update_kernel.sh
    
    Build system: x86_64
    Build-tested: ipq806x/R7800, ath79/generic, bcm27xx/bcm2711
    Run-tested: ipq806x/R7800
    
    No dmesg regressions, everything functional
    
    Signed-off-by: John Audia <graysky@archlinux.us>

--- include/kernel-version.mk
+++ include/kernel-version.mk
@@ -8,11 +8,11 @@ endif
 
 LINUX_VERSION-4.14 = .200
 LINUX_VERSION-4.19 = .149
-LINUX_VERSION-5.4 = .68
+LINUX_VERSION-5.4 = .69
 
 LINUX_KERNEL_HASH-4.14.200 = 5d404a0224a34b5379f1871cc46825487d557c2660459d2b5c3cd4871d699a38
 LINUX_KERNEL_HASH-4.19.149 = d9cdcb9a66942a56b91c8d4b273d15a3c5b8e59fad44b79e03362cfffaa62a00
-LINUX_KERNEL_HASH-5.4.68 = 0e93876c5ae8dc0c55cbe631971a46ab02b90cf7461fed3085703a5e4e3cd6dd
+LINUX_KERNEL_HASH-5.4.69 = a8b31d716b397303a183e42ad525ff2871024a43e3ea530d0fdf73b7f9d27da7
 
 remove_uri_prefix=$(subst git://,,$(subst http://,,$(subst https://,,$(1))))
 sanitize_uri=$(call qstrip,$(subst @,_,$(subst :,_,$(subst .,_,$(subst -,_,$(subst /,_,$(1)))))))
