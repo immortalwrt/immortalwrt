cefad013467815876619c49e59ff422b6c440e1e
Chen Minqiang <ptpt52@gmail.com>
2020-10-14 05:08:52 +0800

    ath79: add support for domywifi_dw33d-nor

--- target/linux/ath79/image/nand.mk
+++ target/linux/ath79/image/nand.mk
@@ -2,6 +2,17 @@ include ./common-netgear.mk	# for netgear-uImage
 
 DEVICE_VARS += RAS_ROOTFS_SIZE RAS_BOARD RAS_VERSION
 
+define Build/append-okli-kernel
+	dd if="$(KDIR)/loader-$(word 1,$(1)).uImage" >> "$@"
+endef
+
+define Build/prepad-okli-kernel
+  -[ -f "$@" ] && \
+  dd if="$(KDIR)/loader-$(word 1,$(1)).uImage" of="$@".tmp bs=64k conv=sync && \
+  cat "$@" >>"$@".tmp && \
+  mv "$@".tmp "$@"
+endef
+
 # attention: only zlib compression is allowed for the boot fs
 define Build/zyxel-buildkerneljffs
 	rm -rf  $(KDIR_TMP)/zyxelnbg6716
@@ -80,6 +91,25 @@ define Device/domywifi_dw33d
 	check-size
 endef
 TARGET_DEVICES += domywifi_dw33d
+
+define Device/domywifi_dw33d-nor
+  $(Device/domywifi_dw33d)
+  DEVICE_VARIANT := NOR
+  IMAGE_SIZE := 14464k
+  BLOCKSIZE := 64k
+  LOADER_TYPE := bin
+  LOADER_FLASH_OFFS := 0x60000
+  COMPILE := loader-$(1).bin loader-$(1).uImage
+  COMPILE/loader-$(1).bin := loader-okli-compile
+  COMPILE/loader-$(1).uImage := append-loader-okli $(1) | pad-to 64k | lzma | uImage lzma
+  KERNEL := kernel-bin | append-dtb | lzma | uImage lzma -M 0x4f4b4c49
+  IMAGES := sysupgrade.bin breed-factory.bin
+  IMAGE/sysupgrade.bin := append-kernel | pad-to $$$$(BLOCKSIZE) | append-rootfs | pad-rootfs | append-metadata | \
+	check-size
+  IMAGE/breed-factory.bin := append-kernel | pad-to $$$$(BLOCKSIZE) | append-rootfs | pad-rootfs | \
+	prepad-okli-kernel $(1) | pad-to 14528k | append-okli-kernel $(1)
+endef
+TARGET_DEVICES += domywifi_dw33d-nor
 
 define Device/glinet_gl-ar300m-common-nand
   SOC := qca9531
