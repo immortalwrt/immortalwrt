b20642d5d4ba5744ab64a1c1e18ee237b1dd79c9
CN_SZTL <cnsztl@project-openwrt.eu.org>
2020-11-13 19:20:20 +0800

    luci-app-ssr-plus: sync with upstream source

--- package/lean/luci-app-ssr-plus/root/usr/share/shadowsocksr/chinaipset.sh
+++ package/lean/luci-app-ssr-plus/root/usr/share/shadowsocksr/chinaipset.sh
@@ -1,11 +1,7 @@
 #!/bin/sh
-LOCK_FILE="/var/lock/ssr-chinaipset.lock"
-[ -f "$LOCK_FILE" ] && exit 2
 [ -f "$1" ] && china_ip=$1
-touch "$LOCK_FILE"
 ipset -! flush china 2>/dev/null
-ipset -! -R <<-EOF || rm -f $LOCK_FILE && exit 1
+ipset -! -R <<-EOF || exit 1
 	create china hash:net
 	$(cat ${china_ip:=/etc/ssr/china_ssr.txt} | sed -e "s/^/add china /")
 EOF
-rm -f $LOCK_FILE
