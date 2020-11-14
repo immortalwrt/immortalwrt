f324a1833c2ab76b7044228b5dc4354c7274bda2
CN_SZTL <cnsztl@project-openwrt.eu.org>
2020-11-15 00:39:33 +0800

    luci-app-turboacc: bug fixes

--- package/ctcgfw/luci-app-turboacc/luasrc/model/cbi/turboacc.lua
+++ package/ctcgfw/luci-app-turboacc/luasrc/model/cbi/turboacc.lua
@@ -46,14 +46,12 @@ end
 if nixio.fs.access("/lib/modules/" .. kernel_version .. "/tcp_bbr.ko") then
 bbr_cca = s:option(Flag, "bbr_cca", translate("BBR CCA"))
 bbr_cca.default = 0
-bbr_cca.rmempty = false
 bbr_cca.description = translate("Using BBR CCA can improve TCP network performance effectively")
 end 
 
 if nixio.fs.access("/lib/modules/" .. kernel_version .. "/xt_FULLCONENAT.ko") then
 fullcone_nat = s:option(Flag, "fullcone_nat", translate("FullCone NAT"))
 fullcone_nat.default = 0
-fullcone_nat.rmempty = false
 fullcone_nat.description = translate("Using FullCone NAT can improve gaming performance effectively")
 end 
 
