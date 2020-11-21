8503518b3e1b5b12066204fe08efc33f82bbfb95
CN_SZTL <cnsztl@project-openwrt.eu.org>
2020-11-21 19:19:54 +0800

    luci-app-ssr-plus: rollback to 180-10 stable version

--- package/lean/luci-app-ssr-plus/root/usr/share/shadowsocksr/ssrplusupdate.sh
+++ package/lean/luci-app-ssr-plus/root/usr/share/shadowsocksr/ssrplusupdate.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 /usr/bin/lua /usr/share/shadowsocksr/update.lua
 sleep 2s
-/usr/share/shadowsocksr/chinaipset.sh /tmp/etc/china_ssr.txt
+/usr/share/shadowsocksr/chinaipset.sh
 sleep 2s
 /usr/bin/lua /usr/share/shadowsocksr/subscribe.lua
