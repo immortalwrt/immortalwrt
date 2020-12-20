83f5e253a2e540248a27607b4aabf9ecd1dec171
Tianling Shen <cnsztl@project-openwrt.eu.org>
2020-12-20 12:49:38 +0800

    Revert "rockchip: make sysupgrade image generation reusable"
    This reverts commit 6039577c0bfd1e6ba1da42ac80d39f737cfb8102.

--- target/linux/rockchip/image/armv8.mk
+++ target/linux/rockchip/image/armv8.mk
@@ -10,8 +10,9 @@ define Device/friendlyarm_nanopi-r2s
   DEVICE_MODEL := NanoPi R2S
   SOC := rk3328
   UBOOT_DEVICE_NAME := nanopi-r2s-rk3328
-  DEVICE_PACKAGES := kmod-usb-net kmod-usb-net-rtl8152
-  $(call Device/FitImageLzma)
+  IMAGE/sysupgrade.img.gz := boot-common | boot-script nanopi-r2s | pine64-img | gzip | append-metadata
+  DEVICE_PACKAGES := kmod-usb-net-rtl8152
+  $(call Devuce/FitImageLzma)
 endef
 TARGET_DEVICES += friendlyarm_nanopi-r2s
 
@@ -20,6 +21,7 @@ define Device/pine64_rockpro64
   DEVICE_MODEL := RockPro64
   SOC := rk3399
   UBOOT_DEVICE_NAME := rockpro64-rk3399
+  IMAGE/sysupgrade.img.gz := boot-common | boot-script | pine64-img | gzip | append-metadata
 endef
 TARGET_DEVICES += pine64_rockpro64
 
