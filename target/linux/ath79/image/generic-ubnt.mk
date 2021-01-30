99ffe186a1cbc2718315bc640486e9ef8dd025e3
AmadeusGhost <42570690+AmadeusGhost@users.noreply.github.com>
2021-01-30 12:20:16 +0800

    ath79: sync official source code

--- target/linux/ath79/image/generic-ubnt.mk
+++ target/linux/ath79/image/generic-ubnt.mk
@@ -335,7 +335,7 @@ endef
 TARGET_DEVICES += ubnt_rocket-m
 
 define Device/ubnt_routerstation_common
-  DEVICE_PACKAGES := -kmod-ath9k -wpad-openssl -uboot-envtools kmod-usb-ohci \
+  DEVICE_PACKAGES := -kmod-ath9k -wpad-basic-wolfssl -uboot-envtools kmod-usb-ohci \
 	kmod-usb2 fconfig
   DEVICE_VENDOR := Ubiquiti
   SOC := ar7161
