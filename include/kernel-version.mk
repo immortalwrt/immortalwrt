616ab0cab9bc7b0c97d192b7d8e100b0b6701bcb
CN_SZTL <cnsztl@project-openwrt.eu.org>
2021-02-17 21:30:07 +0800

    kernel: bump 4.19 to 4.19.176
    No patches need to be refreshed.
    
    Signed-off-by: CN_SZTL <cnsztl@project-openwrt.eu.org>

--- include/kernel-version.mk
+++ include/kernel-version.mk
@@ -8,11 +8,11 @@ endif
 
 LINUX_VERSION-4.9 = .257
 LINUX_VERSION-4.14 = .221
-LINUX_VERSION-4.19 = .175
+LINUX_VERSION-4.19 = .176
 
 LINUX_KERNEL_HASH-4.9.257 = 95e780fffc81de8217a31a3e6e212b69643f5344e9ba0dabaa6e426126f6d64f
 LINUX_KERNEL_HASH-4.14.221 = ce2254075ff4846f13380c1cf4bec1e351e5996a7dd109c200e222e49fd2c7e9
-LINUX_KERNEL_HASH-4.19.175 = ccee4b84ff2b16ad438e013bd211efea12f14e69f7cadb0e095fc89c781b2b25
+LINUX_KERNEL_HASH-4.19.176 = e1c3b8d21ba31bd5c50bb3b2a741f62958eb2a116bf13fa0e14f1656c4826073
 
 remove_uri_prefix=$(subst git://,,$(subst http://,,$(subst https://,,$(1))))
 sanitize_uri=$(call qstrip,$(subst @,_,$(subst :,_,$(subst .,_,$(subst -,_,$(subst /,_,$(1)))))))
