c7a2362ef7765f6b8d329e903f285a9eb725d5a7
Mattraks <16359027+Mattraks@users.noreply.github.com>
2021-02-18 10:41:11 +0800

    luci-app-ssr-plus: bug fixes
    Signed-off-by: CN_SZTL <cnsztl@project-openwrt.eu.org>

--- package/lean/luci-app-ssr-plus/root/usr/share/shadowsocksr/subscribe.lua
+++ package/lean/luci-app-ssr-plus/root/usr/share/shadowsocksr/subscribe.lua
@@ -232,7 +232,7 @@ local function processData(szType, content)
 		local userinfo = hostInfo[1]
 		local password = userinfo
 		result.alias = UrlDecode(alias)
-		result.type = "trojan"
+		result.type = luci.sys.exec('type -t -p trojan') ~= "" and "trojan" or "v2ray"
 		result.v2ray_protocol = "trojan"
 		result.server = host[1]
 		-- 按照官方的建议 默认验证ssl证书
