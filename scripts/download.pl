4aee3088ea692ec448a5443d4c892bfb0b9bffd4
Tianling Shen <cnsztl@immortalwrt.org>
2021-03-22 23:21:13 +0800

    scripts/download.pl: add download mirrors hosted by SuLingGG
    Signed-off-by: Tianling Shen <cnsztl@immortalwrt.org>

--- scripts/download.pl
+++ scripts/download.pl
@@ -290,6 +290,7 @@ foreach my $mirror (@ARGV) {
 }
 
 unshift @mirrors, "http://182.140.223.146/dl";
+unshift @mirrors, "https://openwrt.cc/dl/immortalwrt/master";
 push @mirrors, 'https://sources.cdn.openwrt.org';
 push @mirrors, 'https://sources.openwrt.org';
 push @mirrors, 'https://mirror2.openwrt.org/sources';
