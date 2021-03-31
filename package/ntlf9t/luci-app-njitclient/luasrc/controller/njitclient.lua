5d418c40dde5afdb8025b3e0d17a59dfcb88ef21
Tianling Shen <cnsztl@immortalwrt.org>
2021-03-31 23:15:35 +0800

    ntlf9t: move all LuCI apps to luci feed
    Signed-off-by: Tianling Shen <cnsztl@immortalwrt.org>

--- package/ntlf9t/luci-app-njitclient/luasrc/controller/njitclient.lua
+++
@@ -1,5 +0,0 @@
-module("luci.controller.njitclient", package.seeall)
-
-function index()
-        entry({"admin", "network", "njitclient"}, cbi("njitclient"), _("NJIT Client"), 100)
-        end
--- package/ntlf9t/luci-app-sysuh3c/luasrc/controller/sysuh3c.lua
+++
@@ -1,5 +0,0 @@
-module("luci.controller.sysuh3c", package.seeall)
-
-function index()
-        entry({"admin", "network", "sysuh3c"}, cbi("sysuh3c"), _("SYSU H3C Client"), 100)
-        end
