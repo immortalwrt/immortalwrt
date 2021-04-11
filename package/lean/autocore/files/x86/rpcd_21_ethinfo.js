immortalwrt/immortalwrt

commit 521cdafda6e7af4d23d42c7745770dd2cc47fd67

autocore: ethinfo: adapt LuCI style
Signed-off-by: Tianling Shen <cnsztl@immortalwrt.org>

package/lean/autocore/files/x86/rpcd_21_ethinfo.js
@@ -19,12 +19,12 @@ return L.Class.extend({
 	render: function(data) {
 		var ethinfo = Array.isArray(data[0].ethinfo) ? data[0].ethinfo : [];
 
-		var table = E('div', { 'class': 'table' }, [
-			E('div', { 'class': 'tr table-titles' }, [
-				E('div', { 'class': 'th' }, _('Ethernet Name')),
-				E('div', { 'class': 'th' }, _('Link Status')),
-				E('div', { 'class': 'th' }, _('Speed')),
-				E('div', { 'class': 'th' }, _('Duplex'))
+		var table = E('td', { 'class': 'table' }, [
+			E('tr', { 'class': 'tr table-titles' }, [
+				E('td', { 'class': 'th' }, _('Ethernet Name')),
+				E('td', { 'class': 'th' }, _('Link Status')),
+				E('td', { 'class': 'th' }, _('Speed')),
+				E('td', { 'class': 'th' }, _('Duplex'))
 			])
 		]);
 

commit e9646f92aa7b6b5380ebf931de84a76fcafd5af7

autocore: ethinfo: adapt LuCI style again
Fixes: 521cdafda6e7af4d23d42c7745770dd2cc47fd67 ("autocore: ethinfo: adapt LuCI style")

Signed-off-by: Tianling Shen <cnsztl@immortalwrt.org>

package/lean/autocore/files/x86/rpcd_21_ethinfo.js
@@ -19,12 +19,12 @@ return L.Class.extend({
 	render: function(data) {
 		var ethinfo = Array.isArray(data[0].ethinfo) ? data[0].ethinfo : [];
 
-		var table = E('td', { 'class': 'table' }, [
+		var table = E('table', { 'class': 'table' }, [
 			E('tr', { 'class': 'tr table-titles' }, [
-				E('td', { 'class': 'th' }, _('Ethernet Name')),
-				E('td', { 'class': 'th' }, _('Link Status')),
-				E('td', { 'class': 'th' }, _('Speed')),
-				E('td', { 'class': 'th' }, _('Duplex'))
+				E('th', { 'class': 'th' }, _('Ethernet Name')),
+				E('th', { 'class': 'th' }, _('Link Status')),
+				E('th', { 'class': 'th' }, _('Speed')),
+				E('th', { 'class': 'th' }, _('Duplex'))
 			])
 		]);
 