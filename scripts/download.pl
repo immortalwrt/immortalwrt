9961791b9ec0d245da3f3cdeef55df1fa37442cc
Tianling Shen <cnsztl@immortalwrt.org>
2021-04-17 00:12:02 +0800

    scripts/download.pl: remove unavailable mirror
    Signed-off-by: Tianling Shen <cnsztl@immortalwrt.org>

--- scripts/download.pl
+++ scripts/download.pl
@@ -281,7 +281,6 @@ foreach my $mirror (@ARGV) {
 }
 
 unshift @mirrors, "https://mirror01.download.immortalwrt.eu.org/openwrt-18.06";
-unshift @mirrors, "http://182.140.223.146/dl";
 push @mirrors, "https://openwrt.cc/dl/immortalwrt/openwrt-18.06";
 push @mirrors, 'https://sources.cdn.openwrt.org';
 push @mirrors, 'https://sources.openwrt.org';
