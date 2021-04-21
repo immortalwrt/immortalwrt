immortalwrt/immortalwrt

commit d61bb995c4d0a56ff01936b37fa45b110c8ac4d3

treewide: add ACL dependency annotations to legacy controllers
Signed-off-by: Tianling Shen <cnsztl@immortalwrt.org>

package/lean/luci-app-n2n_v2/luasrc/controller/n2n_v2.lua
@@ -16,6 +16,7 @@ function index()
 	local page
 	page = entry({"admin", "vpn", "n2n_v2"}, cbi("n2n_v2"), _("N2N v2 VPN"), 45)
 	page.dependent = true
+	page..acl_depends = { "luci-app-n2n_v2" }
 end
 
 function n2n_status()

package/lean/luci-app-accesscontrol/luasrc/controller/mia.lua
@@ -5,7 +5,9 @@ function index()
 		return
 	end
 
-	entry({"admin","services","mia"},cbi("mia"),_("Internet Access Schedule Control"),30).dependent=true
+	local page = entry({"admin","services","mia"},cbi("mia"),_("Internet Access Schedule Control"),30)
+	page.dependent = true
+	page.acl_depends = { "luci-app-accesscontrol" }
 	entry({"admin","services","mia","status"},call("act_status")).leaf=true
 
 end

package/lean/luci-app-adbyby-plus/luasrc/controller/adbyby.lua
@@ -6,8 +6,9 @@ function index()
 		return
 	end
 	
-	entry({"admin", "services", "adbyby"}, alias("admin", "services", "adbyby", "base"),_("ADBYBY Plus +"), 9).dependent = true
-	
+	local page = entry({"admin", "services", "adbyby"}, alias("admin", "services", "adbyby", "base"),_("ADBYBY Plus +"), 9)
+	page.dependent = true
+	page.acl_depends = { "luci-app-adbyby" }
 	entry({"admin", "services", "adbyby", "base"}, cbi("adbyby/base"), _("Base Setting"), 10).leaf=true
 	entry({"admin", "services", "adbyby", "advanced"}, cbi("adbyby/advanced"), _("Advance Setting"), 20).leaf=true
 	entry({"admin", "services", "adbyby", "help"}, form("adbyby/help"), _("Plus+ Domain List"), 30).leaf=true

package/lean/luci-app-airplay2/luasrc/controller/shairport-sync.lua
@@ -8,8 +8,9 @@ function index()
 		return
 	end
 
-	entry({"admin", "services", "shairport-sync"}, cbi("shairport-sync"), _("AirPlay 2 Receiver")).dependent = true
-	
+	entry({"admin", "services", "shairport-sync"}, cbi("shairport-sync"), _("AirPlay 2 Receiver"))
+	page.dependent = true
+	page.acl_depends = { "luci-app-airplay2" }
 	entry({"admin","services","shairport-sync","run"},call("act_status")).leaf=true
 
 end

package/lean/luci-app-amule/luasrc/controller/amule.lua
@@ -23,6 +23,7 @@ function index()
         
 	local page = entry({"admin", "nas", "amule"}, cbi("amule"), _("aMule Settings"))
 	page.dependent = true
+	page.acl_depends = { "luci-app-amule" }
 	entry( {"admin", "nas", "amule", "logview"}, call("logread") ).leaf = true
 	entry( {"admin", "nas", "amule", "status"}, call("get_pid") ).leaf = true
 	entry( {"admin", "nas", "amule", "amulecmd"}, call("amulecmd") ).leaf = true

package/lean/luci-app-arpbind/luasrc/controller/arpbind.lua
@@ -9,5 +9,7 @@ function index()
 	if not nixio.fs.access("/etc/config/arpbind") then
 		return
 	end
-	entry({"admin", "network", "arpbind"}, cbi("arpbind"), _("IP/MAC Binding"), 45).dependent = true
+	local page = entry({"admin", "network", "arpbind"}, cbi("arpbind"), _("IP/MAC Binding"), 45)
+	page.dependent = true
+	page.acl_depends = { "luci-app-arpbind" }
 end

package/lean/luci-app-autoreboot/luasrc/controller/autoreboot.lua
@@ -1,4 +1,4 @@
 module("luci.controller.autoreboot",package.seeall)
 function index()
-entry({"admin","system","autoreboot"},cbi("autoreboot"),_("Scheduled Reboot"),88)
+entry({"admin","system","autoreboot"},cbi("autoreboot"),_("Scheduled Reboot"),88).acl_depends = { "luci-app-autoreboot" }
 end

package/lean/luci-app-cifs-mount/luasrc/controller/cifs.lua
@@ -10,4 +10,5 @@ function index()
 
 	page = entry({"admin", "nas", "cifs"}, cbi("cifs"), _("Mount SMB NetShare"))
 	page.dependent = true
+	page.acl_depends = { "luci-app-cifs-mount" }
 end

commit 6bf6d58044187a73c3501416f131a9d94a406a3a

luci-app-n2n_v2: fix typo error
Fixes: d61bb995c4d0a5 ("treewide: add ACL dependency annotations to legacy controllers")

Reported-by: fuqiang03
Signed-off-by: Tianling Shen <cnsztl@immortalwrt.org>

package/lean/luci-app-n2n_v2/luasrc/controller/n2n_v2.lua
@@ -16,7 +16,7 @@ function index()
 	local page
 	page = entry({"admin", "vpn", "n2n_v2"}, cbi("n2n_v2"), _("N2N v2 VPN"), 45)
 	page.dependent = true
-	page..acl_depends = { "luci-app-n2n_v2" }
+	page.acl_depends = { "luci-app-n2n_v2" }
 end
 
 function n2n_status()