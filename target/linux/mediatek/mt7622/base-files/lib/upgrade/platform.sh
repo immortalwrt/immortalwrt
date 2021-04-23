9e6cfcafb6014541865f0402e498f9bb76d407ea
INAGAKI Hiroshi <musashino.open@gmail.com>
2021-04-23 18:32:11 +0900

    mediatek: mt7622: add support for ELECOM WRC-X3200GST3
    ELECOM WRC-X3200GST3 is a 2.4/5 GHz band 11ax (Wi-Fi 6) router, based on
    MT7622B.
    
    Specifications:
    
    - SoC		: MediaTek MT7622B
    - RAM		: DDR3 512 MiB (Nanya NT5CC256M16ER-EK)
    - Flash		: SPI-NAND 128 MiB (Winbond W25N01GVZEIG)
    - WLAN		: 2.4/5 GHz 4T4R
      - 2.4 GHz	: MediaTek MT7622B (SoC)
      - 5 GHz	: MediaTek MT7915A
    - Ethernet	: 5x 10/100/1000 Mbps
      - Switch	: MediaTek MT7531
    - LEDs/Keys	: 6x/4x (2x buttons, 1x slide-switch)
    - UART		: through-hole on PCB
      - J19: 3.3V, GND, TX, RX from power jack side
      - 115200n8
    - Power		: 12 VDC, 1.5 A
    
    Flash instruction using factory image:
    
    1. Boot WRC-X3200GST3 normally with "Router" mode
    2. Access to "http://192.168.2.1/" and open firmware update page
       ("ファームウェア更新")
    3. Select the OpenWrt factory image and click apply ("適用") button
    4. Wait ~120 seconds to complete flashing
    
    MAC Addresses:
    
    LAN    : 04:AB:18:xx:xx:77 (Factory, 0x7FFF4 (hex))
    WAN    : 04:AB:18:xx:xx:78 (Factory, 0x7FFFA (hex))
    2.4 GHz: 04:AB:18:xx:xx:79 (Factory, 0x4     (hex))
    5 GHz  : 04:AB:18:xx:xx:7A (none)
    
    Note:
    
    - currently, there is no "phy1tpt" trigger for 5 GHz wlan (MT7915) in
      "trigger" file of LEDs, use "phy1radio" trigger instead
    
    Signed-off-by: INAGAKI Hiroshi <musashino.open@gmail.com>
    (cherry picked from commit 58b3b557b629f6a2046414e0e1b39189d918133f)

--- target/linux/mediatek/mt7622/base-files/lib/upgrade/platform.sh
+++ target/linux/mediatek/mt7622/base-files/lib/upgrade/platform.sh
@@ -34,6 +34,12 @@ platform_do_upgrade() {
 			nand_do_upgrade "$1"
 		fi
 		;;
+	elecom,wrc-x3200gst3|\
+	mediatek,mt7622-rfb1-ubi|\
+	totolink,a8000ru|\
+	xiaomi,redmi-router-ax6s)
+		nand_do_upgrade "$1"
+		;;
 	linksys,e8450-ubi)
 		CI_KERNPART="fit"
 		nand_do_upgrade "$1"
@@ -45,11 +51,6 @@ platform_do_upgrade() {
 			PART_NAME=firmware1
 		fi
 		default_do_upgrade "$1"
-		;;
-	mediatek,mt7622-rfb1-ubi|\
-	totolink,a8000ru|\
-	xiaomi,redmi-router-ax6s)
-		nand_do_upgrade "$1"
 		;;
 	*)
 		default_do_upgrade "$1"
@@ -69,6 +70,7 @@ platform_check_image() {
 	buffalo,wsr-2533dhp2)
 		buffalo_check_image "$board" "$magic" "$1" || return 1
 		;;
+	elecom,wrc-x3200gst3|\
 	mediatek,mt7622-rfb1-ubi|\
 	totolink,a8000ru|\
 	xiaomi,redmi-router-ax6s)
