470cb5d931fb0151cb3cd3f55725fa47d48875d6
Keith T. Garner <kgarner@kgarner.com>
2021-05-05 09:04:15 -0500

    kernel: add kmod-leds-uleds
    The allows userspace LEDs to be created and controlled. This can be useful
    for testing triggers and can also be used to implement virtual LEDs.
    
    Signed-off-by: Keith T. Garner <kgarner@kgarner.com>
    [squash fixup commit and improve option wording]
    Signed-off-by: Paul Spooren <mail@aparcar.org>

--- package/kernel/linux/modules/leds.mk
+++ package/kernel/linux/modules/leds.mk
@@ -175,3 +175,18 @@ define KernelPackage/leds-pwm/description
 endef
 
 $(eval $(call KernelPackage,leds-pwm))
+
+define KernelPackage/leds-uleds
+  SUBMENU:=$(LEDS_MENU)
+  TITLE:=Userspace LEDs
+  DEPENDS:=@!LINUX_4_9
+  KCONFIG:=CONFIG_LEDS_USER
+  FILES:=$(LINUX_DIR)/drivers/leds/uleds.ko
+  AUTOLOAD:=$(call AutoLoad,60,leds-uleds,1)
+endef
+
+define KernelPackage/leds-uleds/description
+ This option enables support for userspace LEDs.
+endef
+
+$(eval $(call KernelPackage,leds-uleds))
