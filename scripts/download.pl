e821c29ca767f70cb015d7b982903ffbc0e3c281
Tianling Shen <cnsztl@immortalwrt.org>
2021-05-26 22:18:37 +0800

    scripts/download: update mirror url
    Signed-off-by: Tianling Shen <cnsztl@immortalwrt.org>

--- scripts/download.pl
+++ scripts/download.pl
@@ -280,8 +280,8 @@ foreach my $mirror (@ARGV) {
 	}
 }
 
-push @mirrors, "https://mirror01.download.immortalwrt.eu.org/master";
-push @mirrors, "https://mirror02.download.immortalwrt.eu.org/master";
+push @mirrors, "https://mirror01.download.immortalwrt.eu.org";
+push @mirrors, "https://mirror02.download.immortalwrt.eu.org";
 push @mirrors, "https://openwrt.cc/dl/immortalwrt/master";
 push @mirrors, 'https://sources.cdn.openwrt.org';
 push @mirrors, 'https://sources.openwrt.org';
