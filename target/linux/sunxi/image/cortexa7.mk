7d21642ce03306e953707154ebbf122215f40b0c
Daniel Golle <daniel@makrotopia.org>
2021-09-10 23:02:15 +0100

    sunxi: bring up DSA b53 switch on Lamobo R1
    Build cortexa7 subtarget with DSA driver for MDIO-connected Broadcom
    BCM53xxx switches. This is needed for the Lamobo R1 aka. BananaPi
    BPi-R1 board which comes with such a switch IC.
    
    Remove old swconfig driver from target kernel config as the only board
    using it is now supported by the DSA driver.
    
    No changes to device tree are needed as upstream DTS already got a
    DSA switch definition and we are just using that upstream source.
    
    Update default network config of the Lamobo R1 to create lan bridge
    with all 4 lan ports.
    
    Introduce DEVICE_COMPAT_VERSION for the board to inform users about
    having the re-create their network configuration and add device alias
    as Bananapi BPi-R1 while at it.
    
    Signed-off-by: Daniel Golle <daniel@makrotopia.org>

--- target/linux/sunxi/image/cortexa7.mk
+++ target/linux/sunxi/image/cortexa7.mk
@@ -63,7 +63,9 @@ TARGET_DEVICES += friendlyarm_zeropi
 define Device/lamobo_lamobo-r1
   DEVICE_VENDOR := Lamobo
   DEVICE_MODEL := Lamobo R1
-  DEVICE_PACKAGES:=kmod-ata-sunxi kmod-rtl8192cu swconfig wpad-basic-wolfssl
+  DEVICE_ALT0_VENDOR := Bananapi
+  DEVICE_ALT0_MODEL := BPi-R1
+  DEVICE_PACKAGES := kmod-ata-sunxi kmod-rtl8192cu wpad-basic-wolfssl
   SOC := sun7i-a20
 endef
 TARGET_DEVICES += lamobo_lamobo-r1
