40d838ff6bc76440f34f9059927a006c1b2252e9
Robert Marko <robimarko@gmail.com>
2022-01-12 10:36:58 +0300

    ipq806x: add driver for EMC2301 fan controller
    Basic fan controls are working, including PWM and
    tachometer.
    
    RPM target mode is not working yet.
    
    Signed-off-by: Robert Marko <robimarko@gmail.com>
    Signed-off-by: Tianling Shen <cnsztl@immortalwrt.org>

--- target/linux/ipq806x/modules.mk
+++ target/linux/ipq806x/modules.mk
@@ -14,3 +14,19 @@ define KernelPackage/phy-qcom-ipq806x-usb/description
 endef
 
 $(eval $(call KernelPackage,phy-qcom-ipq806x-usb))
+
+
+define KernelPackage/hwmon-emc2305
+  TITLE:=SMSC EMC2301/2/3/5 fan controller
+  KCONFIG:=CONFIG_SENSORS_EMC2305
+  FILES:=$(LINUX_DIR)/drivers/hwmon/emc2305.ko
+  AUTOLOAD:=$(call AutoProbe,emc2305)
+  $(call AddDepends/hwmon,+kmod-i2c-core +kmod-thermal +kmod-regmap-i2c)
+endef
+
+define KernelPackage/hwmon-emc2305/description
+ Kernel module for SMSC EMC2301/EMC2302/EMC2303/EMC2305 fan controllers
+endef
+
+$(eval $(call KernelPackage,hwmon-emc2305))
+
