bb058fa5074c0ed6bb4241a5502c8a9c3bbaa3ca
Dominick Grift <dominick.grift@defensec.nl>
2022-05-01 19:54:04 +0200

    base-files: address sed in-place without SELinux awareness
    sed(1) in busybox does not support this functionality:
    https://git.savannah.gnu.org/cgit/sed.git/tree/sed/execute.c#n598
    
    This causes /etc/group to become mislabeled when a package requests
    that a uid/gid be added on OpenWrt with SELinux
    
    Signed-off-by: Daniel Golle <daniel@makrotopia.org>
    [move restorecon inside lock]
    Signed-off-by: Dominick Grift <dominick.grift@defensec.nl>
    (cherry picked from commit 5109bd164c8f2273329483f990188fb36cf3ad68)

--- package/base-files/files/lib/functions.sh
+++ package/base-files/files/lib/functions.sh
@@ -340,6 +340,7 @@ group_add_user() {
 	[ -n "$(echo $grp | grep ":$")" ] && delim=""
 	[ -n "$IPKG_INSTROOT" ] || lock /var/lock/passwd
 	sed -i "s/$grp/$grp$delim$2/g" ${IPKG_INSTROOT}/etc/group
+	selinuxenabled 2>/dev/null && restorecon /etc/group
 	[ -n "$IPKG_INSTROOT" ] || lock -u /var/lock/passwd
 }
 
