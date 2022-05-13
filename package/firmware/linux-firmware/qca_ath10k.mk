c0f630a98f37e10581df50754443cb68dee4eea9
Christian Lamparter <chunkeey@gmail.com>
2022-05-13 22:07:59 +0200

    linux-firmware: take linux-firmware.git's qca99x0 boardfile
    Kalle Valo managed to add the qca9980's boardfile in the
    upstream repository. Sourcing the file from his repository
    is no longer needed.
    
    Signed-off-by: Christian Lamparter <chunkeey@gmail.com>
    (cherry picked from commit 4ba7f6d9cbd74adc82ab3064cc4c9f6ec5eb51a6)

--- package/firmware/linux-firmware/qca_ath10k.mk
+++ package/firmware/linux-firmware/qca_ath10k.mk
@@ -1,14 +1,3 @@
-QCA99X0_BOARD_REV:=ddcec9efd245da9365c474f513a855a55f3ac7fe
-QCA99X0_BOARD_FILE:=board-2.bin.$(QCA99X0_BOARD_REV)
-
-define Download/qca99x0-board
-  URL:=https://source.codeaurora.org/quic/qsdk/oss/firmware/ath10k-firmware/plain/ath10k/QCA99X0/hw2.0
-  URL_FILE:=board-2.bin?id=$(QCA99X0_BOARD_REV)
-  FILE:=$(QCA99X0_BOARD_FILE)
-  HASH:=03711ac21e60ef59d3815e235eb721c0c22851b5410299411085aa6f2af45401
-endef
-$(eval $(call Download,qca99x0-board))
-
 Package/ath10k-firmware-qca4019 = $(call Package/firmware-default,ath10k qca4019 firmware)
 define Package/ath10k-firmware-qca4019/install
 	$(INSTALL_DIR) $(1)/lib/firmware/ath10k/QCA4019/hw1.0
@@ -80,11 +69,8 @@ Package/ath10k-firmware-qca99x0 = $(call Package/firmware-default,ath10k qca99x0
 define Package/ath10k-firmware-qca99x0/install
 	$(INSTALL_DIR) $(1)/lib/firmware/ath10k/QCA99X0/hw2.0
 	$(INSTALL_DATA) \
-		$(DL_DIR)/$(QCA99X0_BOARD_FILE) \
+		$(PKG_BUILD_DIR)/ath10k/QCA99X0/hw2.0/board-2.bin \
 		$(1)/lib/firmware/ath10k/QCA99X0/hw2.0/board-2.bin
-	$(INSTALL_DATA) \
-		$(PKG_BUILD_DIR)/ath10k/QCA99X0/hw2.0/board.bin \
-		$(1)/lib/firmware/ath10k/QCA99X0/hw2.0/board.bin
 	$(INSTALL_DATA) \
 		$(PKG_BUILD_DIR)/ath10k/QCA99X0/hw2.0/firmware-5.bin \
 		$(1)/lib/firmware/ath10k/QCA99X0/hw2.0/firmware-5.bin
