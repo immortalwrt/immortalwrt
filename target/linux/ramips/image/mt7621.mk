15668d05d785ebf015dd3df0cd114573aa6dbb7f
langliang3241 <49267148+langliang3241@users.noreply.github.com>
2022-07-09 18:31:25 +0800

    修改 IMAGE_SIZE := 32448k 最大化利用闪存空间
    修改 IMAGE_SIZE := 32448k 最大化利用闪存空间

--- target/linux/ramips/image/mt7621.mk
+++ target/linux/ramips/image/mt7621.mk
@@ -900,7 +900,7 @@ endef
 TARGET_DEVICES += jcg_y2
 
 define Device/jdcloud_re-sp-01b
-  IMAGE_SIZE := 27328k
+  IMAGE_SIZE := 32448k
   DEVICE_VENDOR := JDCloud
   DEVICE_MODEL := RE-SP-01B
   DEVICE_PACKAGES := kmod-fs-ext4 kmod-mt7603 kmod-mt7615e kmod-mt7615-firmware \
