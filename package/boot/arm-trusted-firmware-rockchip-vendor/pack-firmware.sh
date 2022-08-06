de8e8348460ee3583e22606e810c75346feb6aa9
Tianling Shen <cnsztl@immortalwrt.org>
2022-08-07 00:25:39 +0800

    arm-trusted-firmware-rockchip-vendor: bump to latest git HEAD
    Signed-off-by: Tianling Shen <cnsztl@immortalwrt.org>

--- package/boot/arm-trusted-firmware-rockchip-vendor/pack-firmware.sh
+++ package/boot/arm-trusted-firmware-rockchip-vendor/pack-firmware.sh
@@ -18,11 +18,11 @@ case "$VARIANT" in
 	LOADER="rk33/rk3399_miniloader_v1.26.bin"
 	;;
 "rk3568")
-	ATF="rk35/rk3568_bl31_v1.33.elf"
+	ATF="rk35/rk3568_bl31_v1.34.elf"
 	DDR="rk35/rk3568_ddr_1560MHz_v1.13.bin"
 	;;
 "rk3588")
-	ATF="rk35/rk3588_bl31_v1.26.elf"
+	ATF="rk35/rk3588_bl31_v1.27.elf"
 	DDR="rk35/rk3588_ddr_lp4_2112MHz_lp5_2736MHz_v1.08.bin"
 	;;
 *)
