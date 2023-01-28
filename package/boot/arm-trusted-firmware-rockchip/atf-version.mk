7b004773c398cbff6133a5390bc583731b5c3715
Tianling Shen <cnsztl@immortalwrt.org>
2023-01-28 23:41:22 +0800

    arm-trusted-firmware-rockchip: rk3568: bump to latest firmware version
    Signed-off-by: Tianling Shen <cnsztl@immortalwrt.org>

--- package/boot/arm-trusted-firmware-rockchip/atf-version.mk
+++ package/boot/arm-trusted-firmware-rockchip/atf-version.mk
@@ -6,7 +6,7 @@ RK3399_ATF:=rk3399_bl31_v1.35.elf
 RK3399_DDR:=rk3399_ddr_800MHz_v1.28.bin
 RK3399_LOADER:=rk3399_miniloader_v1.26.bin
 
-RK3568_ATF:=rk3568_bl31_v1.28.elf
+RK3568_ATF:=rk3568_bl31_v1.36.elf
 RK3568_DDR:=rk3568_ddr_1560MHz_v1.15.bin
 
 RK3588_ATF:=rk3588_bl31_v1.32.elf
