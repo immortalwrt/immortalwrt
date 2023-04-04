bed2c6c773c945a95bf2444d149defb186666c40
Tianling Shen <cnsztl@immortalwrt.org>
2023-04-04 20:05:08 +0800

    rockchip: fix supported device for firefly roc-rk3328-cc
    Signed-off-by: Tianling Shen <cnsztl@immortalwrt.org>

--- target/linux/rockchip/image/armv8.mk
+++ target/linux/rockchip/image/armv8.mk
@@ -5,14 +5,15 @@
 # FIT will be loaded at 0x02080000. Leave 16M for that, align it to 2M and load the kernel after it.
 KERNEL_LOADADDR := 0x03200000
 
-define Device/firefly_roc-cc
+define Device/firefly_roc-rk3328-cc
   DEVICE_VENDOR := Firefly
   DEVICE_MODEL := ROC-RK3328-CC
   SOC := rk3328
+  DEVICE_DTS := rockchip/rk3328-roc-cc
   UBOOT_DEVICE_NAME := roc-cc-rk3328
   IMAGE/sysupgrade.img.gz := boot-common | boot-script | pine64-bin | gzip | append-metadata
 endef
-TARGET_DEVICES += firefly_roc-cc
+TARGET_DEVICES += firefly_roc-rk3328-cc
 
 define Device/friendlyarm_nanopi-r2c
   DEVICE_VENDOR := FriendlyARM
