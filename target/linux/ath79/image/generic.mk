11d0f1d46e1d6c0513d025b28559090af4dc00b0
Tianling Shen <cnsztl@immortalwrt.org>
2023-05-26 17:06:19 +0800

    ath79: disable build for zyxel_nwa1123-ac by default
    Signed-off-by: Tianling Shen <cnsztl@immortalwrt.org>

--- target/linux/ath79/image/generic.mk
+++ target/linux/ath79/image/generic.mk
@@ -3083,6 +3083,7 @@ define Device/zyxel_nwa1123-ac
   ZYXEL_MODEL_STRING := AAOX
   DEVICE_PACKAGES := kmod-ath10k-ct-smallbuffers \
 	ath10k-firmware-qca988x-ct
+  DEFAULT := n
 endef
 TARGET_DEVICES += zyxel_nwa1123-ac
 
