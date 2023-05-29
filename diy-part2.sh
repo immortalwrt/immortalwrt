62572e87520ac942dd9d0d6ad9a4adc3c67ae2dd
Doanduy09 <96837081+Doanduy09@users.noreply.github.com>
2023-05-29 15:56:53 +0700

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -12,3 +12,10 @@
 
 # Modify default IP
 #sed -i 's/192.168.1.1/192.168.50.5/g' package/base-files/files/bin/config_generate
+svn co https://github.com/ofmodemsandmen/ROOterSource2203/trunk/package/rooter-builds/0protocols/luci-proto-3x package/luci-proto-3x
+svn co https://github.com/ofmodemsandmen/ROOterSource2203/trunk/package/rooter-builds/0protocols/luci-proto-mbim package/luci-proto-mbim
+svn co https://github.com/ofmodemsandmen/ROOterSource2203/trunk/package/rooter/0drivers/rqmi package/rqmi
+svn co https://github.com/ofmodemsandmen/ROOterSource2203/trunk/package/rooter/0basicsupport/ext-sms package/ext-sms
+svn co https://github.com/ofmodemsandmen/ROOterSource2203/trunk/package/rooter/0basicsupport/ext-buttons package/ext-buttons
+svn co https://github.com/ofmodemsandmen/ROOterSource2203/trunk/package/rooter/ext-rooter-basic package/ext-rooter-basic
+svn co https://github.com/Roxy09099/ROOterSource2203/trunk/package/rooter/0drivers/rmbim package/rmbim
