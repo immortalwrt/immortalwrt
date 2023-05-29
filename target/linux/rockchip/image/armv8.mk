1c132c165c466793b3f84d70e2bb1e96b7845183
Tianling Shen <cnsztl@immortalwrt.org>
2023-05-29 16:18:08 +0800

    rockchip: add GuangMiao G4C support
    This board was designed as a clone of NanoPi R4S. However, it didn't
    follow the spec and changed the power-supply system massively, and
    because of it there's no way to soft reset this board currently.
    For reboot, you must cut off the power and reengage it.
    
    Signed-off-by: Tianling Shen <cnsztl@immortalwrt.org>

--- target/linux/rockchip/image/armv8.mk
+++ target/linux/rockchip/image/armv8.mk
@@ -96,6 +96,16 @@ define Device/friendlyarm_nanopi-r5s
 endef
 TARGET_DEVICES += friendlyarm_nanopi-r5s
 
+
+define Device/huake_guangmiao-g4c
+  DEVICE_VENDOR := Huake-Cloud
+  DEVICE_MODEL := GuangMiao G4C
+  SOC := rk3399
+  BOOT_FLOW := pine64-bin
+  DEVICE_PACKAGES := kmod-r8168
+endef
+TARGET_DEVICES += huake_guangmiao-g4c
+
 define Device/lunzn_fastrhino-r66s
   DEVICE_VENDOR := Lunzn
   DEVICE_MODEL := FastRhino R66S
