c467ed8d199504a88dac2742b54daab1f91f484c
Doanduy09 <96837081+Doanduy09@users.noreply.github.com>
2023-06-01 23:01:10 +0700

    Add files via upload

--- 99-network-custom.sh
+++ 99-network-custom.sh
@@ -7,24 +7,13 @@ wifi_password="TPTlam011205@!"
 ten_wifi='TPT Lam'
 hostname="DOANDUY"
 
-uci set dropbear.@dropbear[0].PasswordAuth='on'
-uci set dropbear.@dropbear[0].RootPasswordAuth='on'
-hmod -R u=rwX,go= /etc/dropbear
-uci commit dropbear
-/etc/init.d/dropbear restart
-
 sed -i -re 's/^(option check_signature.*)/#\1/g' /etc/opkg.conf
-
-uci set network.lan.ipaddr="192.168.1.1"
-uci commit network
-/etc/init.d/network restart
 
 uci set 'network.lan.ipv6=0'
 uci set 'network.wan.ipv6=0'
 uci set 'dhcp.lan.dhcpv6=disabled'
 /etc/init.d/odhcpd disable
 /etc/init.d/odhcpd stop
-uci commit
 
 uci -q delete dhcp.lan.dhcpv6
 uci -q delete dhcp.lan.ra
@@ -33,12 +22,27 @@ uci commit dhcp
 
 uci set network.lan.delegate="0"
 uci commit network
-/etc/init.d/network restart
 
-uci set base_config.@status[0].SSID='TPT Lam'
+uci set base_config.@status[0].SSID=${ten_wifi}
 uci set base_config.@status[0].SSID_PASSWD=${wifi_password}
 uci set base_config.@status[0].country="CN"
 uci commit base_config
+
+for radio in 'radio0' 'radio1'
+do
+    # Set country
+    uci set wireless."$radio".country="CN"
+
+    # Enable radio
+    uci set wireless."$radio".disabled="0"
+
+    # Set SSID and password
+    uci set wireless.default_"$radio".ssid=${ten_wifi}
+    uci set wireless.default_"$radio".encryption="psk2"
+    uci set wireless.default_"$radio".key=${wifi_password}
+
+    uci -q commit wireless
+done
 
 uci commit wireless
 
@@ -55,25 +59,50 @@ uci add_list system.ntp.server='222.255.146.26'
 
 uci set system.@system[0]=system 
 uci set system.@system[0].hostname="${hostname}"
-uci set system.@system[0].zonename='Asia/Ho_Chi_Minh'
+uci set system.@system[0].zonename='Asia/Ho Chi Minh'
 uci set system.@system[0].timezone='<+07>-7'
 uci commit system
 
+cat << EOI >> /etc/dnsmasq.conf
+
+ipset=/youtube.com/youtube
+ipset=/googlevideo.com/youtube
+ipset=/ytimg.com/youtube
+ipset=/zalo.vn/zalo
+ipset=/zaloapp.com/zalo
+ipset=/zalo.me/zalo
+ipset=/zadn.vn/zalo
+ipset=/tiktok.com/tiktok
+ipset=/tiktokv.com/tiktok
+ipset=/tiktokcdn.com/tiktok
+ipset=/byteicdn.com/tiktok
+
+EOI
+
+/etc/init.d/dnsmasq restart
 
 cat << EOI >> /etc/firewall.include
 
 nft add rule inet fw4 mangle_prerouting iifname wwan0 ip ttl set 65
 nft add rule inet fw4 mangle_postrouting oifname wwan0 ip ttl set 64
+
 nft add rule inet fw4 mangle_prerouting iifname wwan1 ip ttl set 65
 nft add rule inet fw4 mangle_postrouting oifname wwan1 ip ttl set 64
+
+nft add rule inet fw4 mangle_prerouting iifname usb0 ip ttl set 65
+nft add rule inet fw4 mangle_postrouting oifname usb0 ip ttl set 65
+
 
 EOI
 
 cat << EOI >> /etc/firewall.user
+
+iptables -t mangle -I POSTROUTING -o usb0 -j TTL --ttl-set 65
 iptables -t mangle -I POSTROUTING -o wwan0 -j TTL --ttl-set 64
 iptables -t mangle -I POSTROUTING -o wwan1 -j TTL --ttl-set 64
-iptables -t mangle -I POSTROUTING -o usb0 -j TTL --ttl-set 65
+
 EOI
+
 
 # /etc/init.d/network restart
 
