5ccead5965ab15b064adda3c08733d0c1ec480e0
Doanduy09 <96837081+Doanduy09@users.noreply.github.com>
2023-06-12 18:46:18 +0700

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -9,10 +9,12 @@
 # File name: diy-part2.sh
 # Description: OpenWrt DIY script part 2 (After Update feeds)
 #
-
 # Modify default IP
 sed -i 's/192.168.1.1/192.168.1.1/g' package/base-files/files/bin/config_generate
 sed -i '/CONFIG_PACKAGE_glib2=y/d' .config
+echo 'CONFIG_FEED_atinout=y' >>.config
+echo 'CONFIG_PACKAGE_luci-app-atinout-mod=y' >>.config
+echo 'CONFIG_PACKAGE_luci-app-atcommands=y' >>.config
 svn co https://github.com/ofmodemsandmen/ROOterSource2203/trunk/package/rooter-builds/0protocols/luci-proto-3x package/luci-proto-3x
 svn co https://github.com/ofmodemsandmen/ROOterSource2203/trunk/package/rooter-builds/0protocols/luci-proto-mbim package/luci-proto-mbim
 svn co https://github.com/ofmodemsandmen/ROOterSource2203/trunk/package/rooter/0drivers/rqmi package/rqmi
