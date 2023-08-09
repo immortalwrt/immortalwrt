56613c1d911b3c1d6075e89c2fae0cf9ebb061c8
Tianling Shen <cnsztl@immortalwrt.org>
2023-08-09 15:20:57 +0800

    rockchip: remove outdated version switch
    Signed-off-by: Tianling Shen <cnsztl@immortalwrt.org>

--- target/linux/rockchip/modules.mk
+++ target/linux/rockchip/modules.mk
@@ -6,7 +6,7 @@ define KernelPackage/drm-rockchip
   SUBMENU:=$(VIDEO_MENU)
   TITLE:=Rockchip DRM support
   DEPENDS:=@TARGET_rockchip +kmod-backlight +kmod-drm-kms-helper \
-	+kmod-multimedia-input +!LINUX_5_15:kmod-drm-display-helper
+	+kmod-multimedia-input +kmod-drm-display-helper
   KCONFIG:= \
 	CONFIG_DRM_ROCKCHIP \
 	CONFIG_DRM_LOAD_EDID_FIRMWARE=y \
@@ -28,8 +28,7 @@ define KernelPackage/drm-rockchip
 	CONFIG_ROCKCHIP_VOP=y \
 	CONFIG_ROCKCHIP_VOP2=y \
 	CONFIG_DRM_DP_AUX_BUS \
-	CONFIG_DRM_GEM_CMA_HELPER@lt6.1 \
-	CONFIG_DRM_GEM_DMA_HELPER@ge6.1 \
+	CONFIG_DRM_GEM_DMA_HELPER \
 	CONFIG_DRM_PANEL=y \
 	CONFIG_DRM_PANEL_BRIDGE=y \
 	CONFIG_DRM_PANEL_SIMPLE
@@ -38,8 +37,7 @@ define KernelPackage/drm-rockchip
 	$(LINUX_DIR)/drivers/gpu/drm/bridge/synopsys/dw-hdmi-cec.ko \
 	$(LINUX_DIR)/drivers/gpu/drm/bridge/synopsys/dw-mipi-dsi.ko \
 	$(LINUX_DIR)/drivers/phy/rockchip/phy-rockchip-inno-hdmi.ko \
-	$(LINUX_DIR)/drivers/gpu/drm/drm_dp_aux_bus.ko@lt5.19 \
-	$(LINUX_DIR)/drivers/gpu/drm/drm_dma_helper.ko@ge6.1 \
+	$(LINUX_DIR)/drivers/gpu/drm/drm_dma_helper.ko \
 	$(LINUX_DIR)/drivers/gpu/drm/panel/panel-simple.ko \
 	$(LINUX_DIR)/drivers/gpu/drm/rockchip/rockchipdrm.ko \
 	$(LINUX_DIR)/drivers/media/cec/core/cec.ko
