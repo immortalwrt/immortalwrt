ccd122afa5e753ff4152b7587d7e67b4e2903e06
Tianling Shen <cnsztl@immortalwrt.org>
2023-08-12 15:23:40 +0800

    rockchip: add Hilink OPC-H6XK support
    Signed-off-by: Tianling Shen <cnsztl@immortalwrt.org>
    (cherry picked from commit 7ce74cfbc2ecd5c81ceb66c7aeef4712630e474f)

--- target/linux/rockchip/image/armv8.mk
+++ target/linux/rockchip/image/armv8.mk
@@ -34,7 +34,7 @@ define Device/firefly_roc-rk3568-pc
   SUPPORTED_DEVICES := firefly,rk3568-roc-pc
   UBOOT_DEVICE_NAME := roc-pc-rk3568
   BOOT_FLOW := pine64-img
-  DEVICE_PACKAGES := kmod-ata-ahci-platform kmod-brcmfmac wpad-basic-openssl \
+  DEVICE_PACKAGES := kmod-ata-ahci-platform kmod-brcmfmac wpad-openssl \
 	brcmfmac-firmware-43752-sdio brcmfmac-nvram-43752-sdio
 endef
 TARGET_DEVICES += firefly_roc-rk3568-pc
@@ -44,7 +44,7 @@ define Device/friendlyarm_nanopc-t4
   DEVICE_MODEL := NanoPC T4
   SOC := rk3399
   BOOT_FLOW := pine64-bin
-  DEVICE_PACKAGES := kmod-brcmfmac wpad-basic-openssl \
+  DEVICE_PACKAGES := kmod-brcmfmac wpad-openssl \
 	brcmfmac-firmware-4356-sdio brcmfmac-nvram-4356-sdio
 endef
 TARGET_DEVICES += friendlyarm_nanopc-t4
@@ -108,7 +108,7 @@ define Device/friendlyarm_nanopi-r5c
   DEVICE_MODEL := NanoPi R5C
   SOC := rk3568
   BOOT_FLOW := pine64-img
-  DEVICE_PACKAGES := kmod-r8125 kmod-rtw88 rtl8822ce-firmware wpad-basic-openssl
+  DEVICE_PACKAGES := kmod-r8125 kmod-rtw88 rtl8822ce-firmware wpad-openssl
 endef
 TARGET_DEVICES += friendlyarm_nanopi-r5c
 
@@ -120,6 +120,36 @@ define Device/friendlyarm_nanopi-r5s
   DEVICE_PACKAGES := kmod-r8125
 endef
 TARGET_DEVICES += friendlyarm_nanopi-r5s
+
+define Device/hinlink_opc-h66k
+  DEVICE_VENDOR := Hinlink
+  DEVICE_MODEL := OPC-H66K
+  SOC := rk3568
+  BOOT_FLOW := pine64-img
+  DEVICE_PACKAGES := kmod-ata-ahci-platform kmod-hwmon-pwmfan kmod-mt7921e \
+	kmod-r8125 wpad-openssl
+endef
+TARGET_DEVICES += hinlink_opc-h66k
+
+define Device/hinlink_opc-h68k
+  DEVICE_VENDOR := Hinlink
+  DEVICE_MODEL := OPC-H68K
+  SOC := rk3568
+  BOOT_FLOW := pine64-img
+  DEVICE_PACKAGES := kmod-ata-ahci-platform kmod-hwmon-pwmfan kmod-mt7921e \
+	kmod-r8125 wpad-openssl
+endef
+TARGET_DEVICES += hinlink_opc-h68k
+
+define Device/hinlink_opc-h69k
+  DEVICE_VENDOR := Hinlink
+  DEVICE_MODEL := OPC-H69K
+  SOC := rk3568
+  BOOT_FLOW := pine64-img
+  DEVICE_PACKAGES := kmod-ata-ahci-platform kmod-hwmon-pwmfan kmod-mt7916-firmware \
+	kmod-mt7921e kmod-r8125 kmod-usb-serial-option uqmi wpad-openssl
+endef
+TARGET_DEVICES += hinlink_opc-h69k
 
 define Device/huake_guangmiao-g4c
   DEVICE_VENDOR := Huake-Cloud
@@ -164,7 +194,7 @@ define Device/radxa_rock-pi-4a
   SUPPORTED_DEVICES := radxa,rockpi4a radxa,rockpi4
   UBOOT_DEVICE_NAME := rock-pi-4-rk3399
   BOOT_FLOW := pine64-bin
-  DEVICE_PACKAGES := kmod-brcmfmac wpad-basic-openssl \
+  DEVICE_PACKAGES := kmod-brcmfmac wpad-openssl \
 	brcmfmac-firmware-43456-sdio brcmfmac-nvram-43456-sdio
 endef
 TARGET_DEVICES += radxa_rock-pi-4a
