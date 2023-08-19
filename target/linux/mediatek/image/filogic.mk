79875c8dc2ca9998920c07f3512efe19d726a89c
Tianling Shen <cnsztl@immortalwrt.org>
2023-08-19 08:36:44 +0800

    mediatek: add image size for redmi ax6000
    Signed-off-by: Tianling Shen <cnsztl@immortalwrt.org>
    (cherry picked from commit 85f33fceb3b65a04454d8d9b6b907c8bb0b0ca18)

--- target/linux/mediatek/image/filogic.mk
+++ target/linux/mediatek/image/filogic.mk
@@ -571,6 +571,7 @@ define Device/xiaomi_redmi-router-ax6000
   UBINIZE_OPTS := -E 5
   BLOCKSIZE := 128k
   PAGESIZE := 2048
+  IMAGE_SIZE := 112640k
   KERNEL_IN_UBI := 1
   IMAGES += factory.bin
   IMAGE/factory.bin := append-ubi | check-size $$$$(IMAGE_SIZE)
