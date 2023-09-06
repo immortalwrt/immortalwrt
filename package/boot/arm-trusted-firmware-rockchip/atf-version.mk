bd9a5ea1381fbd212a45cda30548d9c388b29b85
Tianling Shen <cnsztl@immortalwrt.org>
2023-09-06 11:37:05 +0800

    arm-trusted-firmware-rockchip: bump to latest git HEAD
    Signed-off-by: Tianling Shen <cnsztl@immortalwrt.org>

--- package/boot/arm-trusted-firmware-rockchip/atf-version.mk
+++ package/boot/arm-trusted-firmware-rockchip/atf-version.mk
@@ -4,10 +4,10 @@ RK3328_LOADER:=rk322xh_miniloader_v2.50.bin
 
 RK3399_ATF:=rk3399_bl31_v1.36.elf
 RK3399_DDR:=rk3399_ddr_800MHz_v1.30.bin
-RK3399_LOADER:=rk3399_miniloader_v1.26.bin
+RK3399_LOADER:=rk3399_miniloader_v1.30.bin
 
-RK3568_ATF:=rk3568_bl31_v1.40.elf
-RK3568_DDR:=rk3568_ddr_1560MHz_v1.16.bin
+RK3568_ATF:=rk3568_bl31_v1.43.elf
+RK3568_DDR:=rk3568_ddr_1560MHz_v1.18.bin
 
-RK3588_ATF:=rk3588_bl31_v1.37.elf
-RK3588_DDR:=rk3588_ddr_lp4_2112MHz_lp5_2736MHz_v1.10.bin
+RK3588_ATF:=rk3588_bl31_v1.41.elf
+RK3588_DDR:=rk3588_ddr_lp4_2112MHz_lp5_2736MHz_v1.13.bin
