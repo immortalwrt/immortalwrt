e0ab5b6aa808202939dae170f75ed3d532b8ab38
Tianling Shen <cnsztl@immortalwrt.org>
2023-09-20 11:07:49 +0800

    rockchip: remove Hinlink support
    Just ask the vendor for firmware.
    
    Signed-off-by: Tianling Shen <cnsztl@immortalwrt.org>

--- target/linux/rockchip/image/armv8.mk
+++ target/linux/rockchip/image/armv8.mk
@@ -153,36 +153,6 @@ define Device/friendlyarm_nanopi-r6s
 endef
 TARGET_DEVICES += friendlyarm_nanopi-r6s
 
-define Device/hinlink_opc-h66k
-  DEVICE_VENDOR := Hinlink
-  DEVICE_MODEL := OPC-H66K
-  SOC := rk3568
-  BOOT_FLOW := pine64-img
-  DEVICE_PACKAGES := kmod-ata-ahci-platform kmod-hwmon-pwmfan kmod-mt7921e \
-	kmod-r8125 wpad-basic-openssl
-endef
-TARGET_DEVICES += hinlink_opc-h66k
-
-define Device/hinlink_opc-h68k
-  DEVICE_VENDOR := Hinlink
-  DEVICE_MODEL := OPC-H68K
-  SOC := rk3568
-  BOOT_FLOW := pine64-img
-  DEVICE_PACKAGES := kmod-ata-ahci-platform kmod-hwmon-pwmfan kmod-mt7921e \
-	kmod-r8125 wpad-basic-openssl
-endef
-TARGET_DEVICES += hinlink_opc-h68k
-
-define Device/hinlink_opc-h69k
-  DEVICE_VENDOR := Hinlink
-  DEVICE_MODEL := OPC-H69K
-  SOC := rk3568
-  BOOT_FLOW := pine64-img
-  DEVICE_PACKAGES := kmod-ata-ahci-platform kmod-hwmon-pwmfan kmod-mt7916-firmware \
-	kmod-mt7921e kmod-r8125 kmod-usb-serial-option uqmi wpad-basic-openssl
-endef
-TARGET_DEVICES += hinlink_opc-h69k
-
 define Device/huake_guangmiao-g4c
   DEVICE_VENDOR := Huake-Cloud
   DEVICE_MODEL := GuangMiao G4C
