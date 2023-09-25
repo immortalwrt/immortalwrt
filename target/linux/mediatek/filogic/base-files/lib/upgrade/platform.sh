76bc8200cc3d7357ebf7f832ee75c5988b49bd4e
Tianling Shen <cnsztl@immortalwrt.org>
2023-09-25 11:35:11 +0800

    mediatek: add CMCC RAX3000M eMMC custom U-Boot layout
    Signed-off-by: Tianling Shen <cnsztl@immortalwrt.org>

--- target/linux/mediatek/filogic/base-files/lib/upgrade/platform.sh
+++ target/linux/mediatek/filogic/base-files/lib/upgrade/platform.sh
@@ -93,6 +93,11 @@ platform_do_upgrade() {
 			;;
 		esac
 		;;
+	cmcc,rax3000m-emmc-ubootmod)
+		CI_KERNPART="kernel"
+		CI_ROOTPART="rootfs"
+		emmc_do_upgrade "$1"
+		;;
 	cudy,wr3000-v1)
 		default_do_upgrade "$1"
 		;;
@@ -164,6 +169,7 @@ platform_copy_config() {
 			;;
 		esac
 		;;
+	cmcc,rax3000m-emmc-ubootmod|\
 	ubnt,unifi-6-plus)
 		emmc_copy_config
 		;;
