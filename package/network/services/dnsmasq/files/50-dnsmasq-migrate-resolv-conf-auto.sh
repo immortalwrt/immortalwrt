26347ab759cc0c171558dfbb5df4411933cf76ca
Tianling Shen <cnsztl@immortalwrt.org>
2024-02-20 22:22:30 +0800

    dnsmasq: add mini_ttl remove migration
    Signed-off-by: Tianling Shen <cnsztl@immortalwrt.org>

--- package/network/services/dnsmasq/files/50-dnsmasq-migrate-resolv-conf-auto.sh
+++ package/network/services/dnsmasq/files/50-dnsmasq-migrate-resolv-conf-auto.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
 
+uci -q get dhcp.@dnsmasq[0].mini_ttl >"/dev/null" && uci -q delete dhcp.@dnsmasq[0].mini_ttl
+
 [ "$(uci get dhcp.@dnsmasq[0].resolvfile)" = "/tmp/resolv.conf.auto" ] && {
 	uci set dhcp.@dnsmasq[0].resolvfile="/tmp/resolv.conf.d/resolv.conf.auto"
 	uci commit dhcp
