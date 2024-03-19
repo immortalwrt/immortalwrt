47c019e2ef965010d80d1788f8aa81f155789003
Tianling Shen <cnsztl@immortalwrt.org>
2024-03-19 14:14:13 +0800

    ramips: disable build for ubnt edgerouter by default
    Signed-off-by: Tianling Shen <cnsztl@immortalwrt.org>

--- target/linux/ramips/image/mt7621.mk
+++ target/linux/ramips/image/mt7621.mk
@@ -2527,6 +2527,7 @@ define Device/ubnt_edgerouter_common
 	ubnt-erx-factory-image $(KDIR)/tmp/$$(KERNEL_INITRAMFS_PREFIX)-factory.tar
   IMAGE/sysupgrade.bin := sysupgrade-tar | append-metadata
   DEVICE_PACKAGES += -wpad-basic-openssl -uboot-envtools
+  DEFAULT := n
 endef
 
 define Device/ubnt_edgerouter-x
