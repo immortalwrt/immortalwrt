immortalwrt/immortalwrt

commit ee3c5fdbf2e080311b5edc75e10ce013a946ea6c

arm-trusted-firmware-rockchip: bump to latest git HEAD
Signed-off-by: Tianling Shen <cnsztl@immortalwrt.org>

package/boot/arm-trusted-firmware-rockchip/atf-version.mk
@@ -6,11 +6,11 @@ RK3399_ATF:=rk3399_bl31_v1.36.elf
 RK3399_TPL:=rk3399_ddr_800MHz_v1.30.bin
 RK3399_LOADER:=rk3399_miniloader_v1.30.bin
 
-RK3566_ATF:=rk3568_bl31_v1.43.elf
-RK3566_TPL:=rk3566_ddr_1056MHz_v1.18.bin
+RK3566_ATF:=rk3568_bl31_v1.44.elf
+RK3566_TPL:=rk3566_ddr_1056MHz_v1.21.bin
 
-RK3568_ATF:=rk3568_bl31_v1.43.elf
-RK3568_TPL:=rk3568_ddr_1560MHz_v1.18.bin
+RK3568_ATF:=rk3568_bl31_v1.44.elf
+RK3568_TPL:=rk3568_ddr_1560MHz_v1.21.bin
 
-RK3588_ATF:=rk3588_bl31_v1.41.elf
-RK3588_TPL:=rk3588_ddr_lp4_2112MHz_lp5_2736MHz_v1.13.bin
+RK3588_ATF:=rk3588_bl31_v1.45.elf
+RK3588_TPL:=rk3588_ddr_lp4_2112MHz_lp5_2736MHz_v1.16.bin

package/boot/arm-trusted-firmware-rockchip/Makefile
@@ -8,10 +8,10 @@ PKG_NAME:=rkbin
 PKG_RELEASE:=1
 
 PKG_SOURCE_PROTO:=git
-PKG_SOURCE_URL:=https://gitlab.com/rk3588_linux/rk/rkbin.git
-PKG_SOURCE_DATE:=2023-08-17
-PKG_SOURCE_VERSION:=1a417bbf7d05c3491ee9613be12a9905fbb8ccb7
-PKG_MIRROR_HASH:=2fef43192127a414e47d38833a74f76d29eddbf0467b6457db7b5de5f997fc12
+PKG_SOURCE_URL:=https://github.com/rockchip-linux/rkbin.git
+PKG_SOURCE_DATE:=2024-02-22
+PKG_SOURCE_VERSION:=a2a0b89b6c8c612dca5ed9ed8a68db8a07f68bc0
+PKG_MIRROR_HASH:=39f15e5f8fac02026065b6747b355b93f4e06202783ae448c43607763211597c
 
 PKG_MAINTAINER:=Tianling Shen <cnsztl@immortalwrt.org>
 

commit bb1eccfba77f5317680ff7b0c6d4dae64fad81f6

arm-trusted-firmware-rockchip: update filename of RK3588's TPL
The RAM frequency was decreased to 2400MHz for stability in v1.16.

Fixes: ee3c5fdbf2e0 ("arm-trusted-firmware-rockchip: bump to latest git HEAD")
Signed-off-by: Tianling Shen <cnsztl@immortalwrt.org>

package/boot/arm-trusted-firmware-rockchip/atf-version.mk
@@ -13,4 +13,4 @@ RK3568_ATF:=rk3568_bl31_v1.44.elf
 RK3568_TPL:=rk3568_ddr_1560MHz_v1.21.bin
 
 RK3588_ATF:=rk3588_bl31_v1.45.elf
-RK3588_TPL:=rk3588_ddr_lp4_2112MHz_lp5_2736MHz_v1.16.bin
+RK3588_TPL:=rk3588_ddr_lp4_2112MHz_lp5_2400MHz_v1.16.bin