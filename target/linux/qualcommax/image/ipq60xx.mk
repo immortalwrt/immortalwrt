3b9a3b6efd5e04e2a8923a5e9ceafce7f97fb5d9
qlljcf666 <60305000+qlljcf666@users.noreply.github.com>
2024-04-23 17:09:34 +0800

    Update ipq60xx.mk

--- target/linux/qualcommax/image/ipq60xx.mk
+++ target/linux/qualcommax/image/ipq60xx.mk
@@ -24,31 +24,6 @@ define Device/netgear_wax214
 endef
 TARGET_DEVICES += netgear_wax214
 
-define Device/FitImage
-	KERNEL_SUFFIX := -fit-uImage.itb
-	KERNEL = kernel-bin | gzip | fit gzip $$(DEVICE_DTS_DIR)/$$(DEVICE_DTS).dtb
-	KERNEL_NAME := Image
-endef
-
-define Device/FitImageLzma
-	KERNEL_SUFFIX := -fit-uImage.itb
-	KERNEL = kernel-bin | lzma | fit lzma $$(DEVICE_DTS_DIR)/$$(DEVICE_DTS).dtb
-	KERNEL_NAME := Image
-endef
-
-define Device/FitzImage
-	KERNEL_SUFFIX := -fit-zImage.itb
-	KERNEL = kernel-bin | fit none $$(DEVICE_DTS_DIR)/$$(DEVICE_DTS).dtb
-	KERNEL_NAME := zImage
-endef
-
-define Device/UbiFit
-	KERNEL_IN_UBI := 1
-	IMAGES := nand-factory.ubi nand-sysupgrade.bin
-	IMAGE/nand-factory.ubi := append-ubi
-	IMAGE/nand-sysupgrade.bin := sysupgrade-tar | append-metadata
-endef
-
 define Device/wf_hr6001
 	$(call Device/FitImage)
 	$(call Device/UbiFit)
