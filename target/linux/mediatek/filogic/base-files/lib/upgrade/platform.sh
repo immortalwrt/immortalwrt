f1b4ec1b360f49e949bc188b252fa1e71a1b1bd8
Tianling Shen <cnsztl@immortalwrt.org>
2024-06-15 22:07:39 +0800

    mediatek: add TP-Link TL-XTR8488 support
    The TP-Link TL-XTR8488 is silimiar to TL-XDR6088 with following changes:
    * 1GB DDR4 RAM
    * Addtional MT7915AN for 5.8 GHz wireless
    
    For flash instructions, see the commit for TP-Link TL-XDR6088.
    
    Based on An Yun's work.
    
    Signed-off-by: Tianling Shen <cnsztl@immortalwrt.org>
    (cherry picked from commit 616be733d07461bc9530305f45be146c76e4977b)

--- target/linux/mediatek/filogic/base-files/lib/upgrade/platform.sh
+++ target/linux/mediatek/filogic/base-files/lib/upgrade/platform.sh
@@ -127,6 +127,7 @@ platform_do_upgrade() {
 	tplink,tl-xdr4288|\
 	tplink,tl-xdr6086|\
 	tplink,tl-xdr6088|\
+	tplink,tl-xtr8488|\
 	xiaomi,mi-router-ax3000t-ubootmod|\
 	xiaomi,mi-router-wr30u-ubootmod|\
 	xiaomi,redmi-router-ax6000-ubootmod)
