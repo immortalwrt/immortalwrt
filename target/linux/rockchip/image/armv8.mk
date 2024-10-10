9d6da3da08fd5d09e124aa4803e426a094b749ff
leeson <97571961+leesonaa@users.noreply.github.com>
2024-10-10 15:39:18 +0800

    rockchip:add HinLink H68K support
    Hardware
    ------------
    RockChip RK3568 ARM64 (4 cores)
    2GB or 4GB LPDDR4X RAM
    2x 1000 Base-T
    2x 2500 Base-T
    32GB eMMC on-board
    Micro-SD Slot
    USB 2.0 Port & USB 3.0 Port
    WIFI MT7921(only pro)/MT7916(only max)
    ------------
    Installation
    -----------
    Uncompress the ImmortalWrt sysupgrade and write it to a micro SD card using dd.

--- target/linux/rockchip/image/armv8.mk
+++ target/linux/rockchip/image/armv8.mk
@@ -148,6 +148,15 @@ define Device/friendlyarm_nanopi-r5s
 endef
 TARGET_DEVICES += friendlyarm_nanopi-r5s
 
+define Device/hinlink_opc-h68k
+  DEVICE_VENDOR := HINLINK
+  DEVICE_MODEL := OPC-H68K
+  SOC := rk3568
+  BOOT_FLOW := pine64-img
+  DEVICE_PACKAGES := kmod-ata-ahci-platform kmod-hwmon-pwmfan kmod-mt7921e kmod-r8125 wpad-openssl
+endef
+TARGET_DEVICES += hinlink_opc-h68k
+
 define Device/huake_guangmiao-g4c
   DEVICE_VENDOR := Huake-Cloud
   DEVICE_MODEL := GuangMiao G4C
