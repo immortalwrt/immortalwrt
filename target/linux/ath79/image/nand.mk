6a73dae98c9c35d38b4756ad24f40e97fb6fbe30
Tianling Shen <cnsztl@immortalwrt.org>
2025-02-05 16:32:41 +0800

    ath79: disable build for meraki mr18 by default
    Signed-off-by: Tianling Shen <cnsztl@immortalwrt.org>

--- target/linux/ath79/image/nand.mk
+++ target/linux/ath79/image/nand.mk
@@ -318,6 +318,7 @@ define Device/meraki_mr18
   KERNEL_INITRAMFS := $$(KERNEL)
   IMAGE/sysupgrade.bin := sysupgrade-tar | append-metadata
   SUPPORTED_DEVICES += mr18
+  DEFAULT := n
 endef
 TARGET_DEVICES += meraki_mr18
 
