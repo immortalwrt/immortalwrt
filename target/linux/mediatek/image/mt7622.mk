70564dc9ab0d6604f199810ac035eb5b7bdfe064
Tianling Shen <cnsztl@immortalwrt.org>
2025-07-10 16:20:24 +0800

    mediatek: add TP-Link TL-XDR3250 v1 support
    Specifications:
      - SoC: Mediatek MT7622B
      - RAM: 256M
      - Flash: EN25QH128 16M
      - Ethernet: RTL8367S 3xGE
      - WiFi: MT7622 2.4G 4x4 + MT7905 5G 4x4
      - UART: 3.3v, 115200n8
    
    Flash instruction
    Please refer commit 9f73519e62e8 ("mediatek: add support for TP-Link TL-XDR3230 v1").
    This board is basically same as XDR3230 v1 so you can reuse the BL2 and FIP for it.
    
    Signed-off-by: Tianling Shen <cnsztl@immortalwrt.org>

--- target/linux/mediatek/image/mt7622.mk
+++ target/linux/mediatek/image/mt7622.mk
@@ -337,11 +337,8 @@ define Device/totolink_a8000ru
 endef
 TARGET_DEVICES += totolink_a8000ru
 
-define Device/tplink_tl-xdr3230-v1
+define Device/tplink_tl-xdr-common
   DEVICE_VENDOR := TP-Link
-  DEVICE_MODEL := TL-XDR3230
-  DEVICE_VARIANT := v1
-  DEVICE_DTS := mt7622-tplink_tl-xdr3230-v1
   DEVICE_DTS_DIR := ../dts
   DEVICE_PACKAGES := kmod-mt7915-firmware swconfig
   KERNEL := kernel-bin | lzma
@@ -352,7 +349,22 @@ define Device/tplink_tl-xdr3230-v1
   IMAGE/sysupgrade.itb := append-kernel | \
 	fit lzma $$(KDIR)/image-$$(firstword $$(DEVICE_DTS)).dtb external-static-with-rootfs | pad-rootfs | append-metadata
 endef
+
+define Device/tplink_tl-xdr3230-v1
+  DEVICE_MODEL := TL-XDR3230
+  DEVICE_VARIANT := v1
+  DEVICE_DTS := mt7622-tplink-tl-xdr3230-v1
+  $(call Device/tplink_tl-xdr-common)
+endef
 TARGET_DEVICES += tplink_tl-xdr3230-v1
+
+define Device/tplink_tl-xdr3250-v1
+  DEVICE_MODEL := TL-XDR3250
+  DEVICE_VARIANT := v1
+  DEVICE_DTS := mt7622-tplink-tl-xdr3250-v1
+  $(call Device/tplink_tl-xdr-common)
+endef
+TARGET_DEVICES += tplink_tl-xdr3250-v1
 
 define Device/ubnt_unifi-6-lr-v1
   DEVICE_VENDOR := Ubiquiti
