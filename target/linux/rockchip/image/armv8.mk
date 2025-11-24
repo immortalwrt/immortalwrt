immortalwrt/immortalwrt

commit 1ab1673689393c2d6a8e0e2e2cc78979fd2be249

rockchip: backport upstreamed dts for 9Tripod X3568 v4
Signed-off-by: Tianling Shen <cnsztl@immortalwrt.org>
(cherry picked from commit 2afe6a18ba611ad71598f26c1d7edef7bb7c30d2)

target/linux/rockchip/image/armv8.mk
@@ -10,6 +10,22 @@ define Device/IfnameMigration
   DEVICE_COMPAT_MESSAGE := Network interface names have been changed
 endef
 
+define Device/9tripod_x3568-v4
+  $(Device/rk3568)
+  DEVICE_VENDOR := 9Tripod
+  DEVICE_MODEL := X3568
+  DEVICE_VARIANT := v4
+  SOC := rk3568
+  DEVICE_DTS := rk3568-9tripod-x3568-v4
+  BOOT_FLOW := pine64-img
+  SUPPORTED_DEVICES := ninetripod,x3568-v4
+  DEVICE_PACKAGES := blkdiscard block-mount kmod-ata-ahci-dwc kmod-nvme kmod-hwmon-pwmfan \
+	kmod-input-adc-keys kmod-saradc-rockchip kmod-rtc-pcf8563 kmod-brcmfmac wpad-openssl \
+	brcmfmac-firmware-43752-sdio brcmfmac-nvram-43752-sdio
+  UBOOT_DEVICE_NAME := 9tripod-x3568-v4-rk3568
+endef
+TARGET_DEVICES += 9tripod_x3568-v4
+
 define Device/ariaboard_photonicat
   DEVICE_VENDOR := Ariaboard
   DEVICE_MODEL := Photonicat
@@ -258,18 +274,6 @@ define Device/mmbox_anas3035
   DEVICE_PACKAGES := kmod-r8125 kmod-ata-ahci-dwc kmod-hwmon-pwmfan kmod-hwmon-drivetemp
 endef
 TARGET_DEVICES += mmbox_anas3035
-
-define Device/ninetripod_x3568-v4
-  DEVICE_VENDOR := NineTripod
-  DEVICE_MODEL := X3568
-  DEVICE_VARIANT := v4
-  SOC := rk3568
-  BOOT_FLOW := pine64-img
-  DEVICE_PACKAGES := blkdiscard block-mount kmod-ata-ahci-dwc kmod-nvme kmod-hwmon-pwmfan \
-	kmod-input-adc-keys kmod-saradc-rockchip kmod-rtc-pcf8563 kmod-brcmfmac wpad-openssl \
-	brcmfmac-firmware-43752-sdio brcmfmac-nvram-43752-sdio
-endef
-TARGET_DEVICES += ninetripod_x3568-v4
 
 define Device/nlnet_xiguapi-v3
   DEVICE_VENDOR := NLnet

package/boot/uboot-rockchip/Makefile
@@ -222,6 +222,13 @@ define U-Boot/rk3568/Default
   TPL:=$(RK3568_TPL)
 endef
 
+define U-Boot/9tripod-x3568-v4-rk3568
+  $(U-Boot/rk3568/Default)
+  NAME:=9Tripod X3568 v4
+  BUILD_DEVICES:= \
+    9tripod_x3568-v4
+endef
+
 define U-Boot/bpi-r2-pro-rk3568
   $(U-Boot/rk3568/Default)
   NAME:=Bananapi-R2 Pro
@@ -276,13 +283,6 @@ define U-Boot/nanopi-r5s-rk3568
   NAME:=NanoPi R5S
   BUILD_DEVICES:= \
     friendlyarm_nanopi-r5s
-endef
-
-define U-Boot/x3568-v4-rk3568
-  $(U-Boot/rk3568/Default)
-  NAME:=NineTripod X3568 v4
-  BUILD_DEVICES:= \
-    ninetripod_x3568-v4
 endef
 
 define U-Boot/photonicat-rk3568
@@ -433,6 +433,7 @@ UBOOT_TARGETS := \
   radxa-cm3-io-rk3566 \
   radxa-zero-3-rk3566 \
   rock-3c-rk3566 \
+  9tripod-x3568-v4-rk3568 \
   bpi-r2-pro-rk3568 \
   fastrhino-r66s-rk3568 \
   fastrhino-r68s-rk3568 \
@@ -442,7 +443,6 @@ UBOOT_TARGETS := \
   nanopi-r5c-rk3568 \
   nanopi-r5s-rk3568 \
   photonicat-rk3568 \
-  x3568-v4-rk3568 \
   xiguapi-v3-rk3568 \
   radxa-e25-rk3568 \
   roc-pc-rk3568 \

package/boot/uboot-rockchip/src/arch/arm/dts/rk3568-9tripod-x3568-v4.dts
@@ -9,8 +9,8 @@
 #include "rk3568.dtsi"
 
 / {
-	model = "NineTripod X3568 v4";
-	compatible = "ninetripod,x3568-v4", "rockchip,rk3568";
+	model = "9Tripod X3568 v4";
+	compatible = "9tripod,x3568-v4", "rockchip,rk3568";
 
 	aliases {
 		ethernet0 = &gmac0;
@@ -18,6 +18,7 @@
 		mmc0 = &sdhci;
 		mmc1 = &sdmmc0;
 		mmc2 = &sdmmc2;
+		rtc0 = &rtc0;
 	};
 
 	chosen {
@@ -170,15 +171,15 @@
 
 	vcc3v3_pcie: regulator-vcc3v3-pcie {
 		compatible = "regulator-fixed";
-		regulator-name = "vcc3v3_pcie";
 		enable-active-high;
-		regulator-min-microvolt = <3300000>;
-		regulator-max-microvolt = <3300000>;
+		gpio = <&gpio3 RK_PC3 GPIO_ACTIVE_HIGH>;
 		pinctrl-names = "default";
 		pinctrl-0 = <&vcc3v3_pcie_en_pin>;
-		gpio = <&gpio0 RK_PD4 GPIO_ACTIVE_HIGH>;
+		regulator-name = "vcc3v3_pcie";
+		regulator-min-microvolt = <3300000>;
+		regulator-max-microvolt = <3300000>;
 		startup-delay-us = <5000>;
-		vin-supply = <&vcc5v0_sys>;
+		vin-supply = <&dc_12v>;
 	};
 
 	vcc5v0_sys: regulator-vcc5v0-sys {
@@ -264,11 +265,10 @@
 	clock_in_out = "output";
 	pinctrl-names = "default";
 	pinctrl-0 = <&gmac0_miim
-			&gmac0_tx_bus2
-			&gmac0_rx_bus2
-			&gmac0_rgmii_clk
-			&gmac0_rgmii_bus
-			&gmac0_clkinout>;
+		     &gmac0_tx_bus2
+		     &gmac0_rx_bus2
+		     &gmac0_rgmii_clk
+		     &gmac0_rgmii_bus>;
 	phy-handle = <&rgmii_phy0>;
 	phy-mode = "rgmii-id";
 	status = "okay";
@@ -281,11 +281,10 @@
 	clock_in_out = "output";
 	pinctrl-names = "default";
 	pinctrl-0 = <&gmac1m1_miim
-			&gmac1m1_tx_bus2
-			&gmac1m1_rx_bus2
-			&gmac1m1_rgmii_clk
-			&gmac1m1_rgmii_bus
-			&gmac1m1_clkinout>;
+		     &gmac1m1_tx_bus2
+		     &gmac1m1_rx_bus2
+		     &gmac1m1_rgmii_clk
+		     &gmac1m1_rgmii_bus>;
 	phy-handle = <&rgmii_phy1>;
 	phy-mode = "rgmii-id";
 	status = "okay";
@@ -558,7 +557,7 @@
 &i2c5 {
 	status = "okay";
 
-	rtc@51 {
+	rtc0: rtc@51 {
 		compatible = "nxp,pcf8563";
 		reg = <0x51>;
 		#clock-cells = <0>;
@@ -580,11 +579,6 @@
 	status = "okay";
 };
 
-/* Required remotectl for IR receiver */
-&pwm7 {
-	status = "disabled";
-};
-
 &mdio0 {
 	rgmii_phy0: ethernet-phy@3 {
 		compatible = "ethernet-phy-ieee802.3-c22";
@@ -592,6 +586,26 @@
 		reset-assert-us = <20000>;
 		reset-deassert-us = <100000>;
 		reset-gpios = <&gpio2 RK_PD3 GPIO_ACTIVE_LOW>;
+
+		leds {
+			#address-cells = <1>;
+			#size-cells = <0>;
+
+			led@1 {
+				reg = <1>;
+				color = <LED_COLOR_ID_GREEN>;
+				function = LED_FUNCTION_LAN;
+				default-state = "keep";
+			};
+
+			/* Note: The LED polarity is inverted */
+			led@2 {
+				reg = <2>;
+				color = <LED_COLOR_ID_AMBER>;
+				function = LED_FUNCTION_LAN;
+				default-state = "keep";
+			};
+		};
 	};
 };
 
@@ -602,6 +616,26 @@
 		reset-assert-us = <20000>;
 		reset-deassert-us = <100000>;
 		reset-gpios = <&gpio2 RK_PD1 GPIO_ACTIVE_LOW>;
+
+		leds {
+			#address-cells = <1>;
+			#size-cells = <0>;
+
+			led@1 {
+				reg = <1>;
+				color = <LED_COLOR_ID_GREEN>;
+				function = LED_FUNCTION_LAN;
+				default-state = "keep";
+			};
+
+			/* Note: The LED polarity is inverted */
+			led@2 {
+				reg = <2>;
+				color = <LED_COLOR_ID_AMBER>;
+				function = LED_FUNCTION_LAN;
+				default-state = "keep";
+			};
+		};
 	};
 };
 
@@ -630,8 +664,7 @@
 
 	pmic {
 		pmic_int: pmic_int {
-			rockchip,pins =
-				<0 RK_PA3 RK_FUNC_GPIO &pcfg_pull_up>;
+			rockchip,pins = <0 RK_PA3 RK_FUNC_GPIO &pcfg_pull_up>;
 		};
 	};
 
@@ -655,7 +688,7 @@
 			rockchip,pins = <2 RK_PD6 RK_FUNC_GPIO &pcfg_pull_none>;
 		};
 		vcc3v3_pcie_en_pin: vcc3v3-pcie-en-pin {
-			rockchip,pins = <0 RK_PD4 RK_FUNC_GPIO &pcfg_pull_none>;
+			rockchip,pins = <3 RK_PC3 RK_FUNC_GPIO &pcfg_pull_none>;
 		};
 	};
 };
@@ -719,7 +752,6 @@
 /* used for AP6275S WiFi */
 &sdmmc2 {
 	bus-width = <4>;
-	disable-wp;
 	cap-sd-highspeed;
 	cap-sdio-irq;
 	keep-power-in-suspend;
@@ -727,9 +759,6 @@
 	non-removable;
 	pinctrl-names = "default";
 	pinctrl-0 = <&sdmmc2m0_bus4 &sdmmc2m0_cmd &sdmmc2m0_clk>;
-	sd-uhs-sdr12;
-	sd-uhs-sdr25;
-	sd-uhs-sdr50;
 	sd-uhs-sdr104;
 	vmmc-supply = <&vcc3v3_sys>;
 	vqmmc-supply = <&vcc_1v8>;

package/boot/uboot-rockchip/src/configs/9tripod-x3568-v4-rk3568_defconfig
@@ -3,7 +3,7 @@ CONFIG_SKIP_LOWLEVEL_INIT=y
 CONFIG_SYS_HAS_NONCACHED_MEMORY=y
 CONFIG_COUNTER_FREQUENCY=24000000
 CONFIG_ARCH_ROCKCHIP=y
-CONFIG_DEFAULT_DEVICE_TREE="rk3568-x3568-v4"
+CONFIG_DEFAULT_DEVICE_TREE="rk3568-9tripod-x3568-v4"
 CONFIG_ROCKCHIP_RK3568=y
 CONFIG_SPL_SERIAL=y
 CONFIG_DEBUG_UART_BASE=0xFE660000
@@ -17,7 +17,7 @@ CONFIG_FIT_VERBOSE=y
 CONFIG_SPL_FIT_SIGNATURE=y
 CONFIG_SPL_LOAD_FIT=y
 CONFIG_LEGACY_IMAGE_FORMAT=y
-CONFIG_DEFAULT_FDT_FILE="rockchip/rk3568-x3568-v4.dtb"
+CONFIG_DEFAULT_FDT_FILE="rockchip/rk3568-9tripod-x3568-v4.dtb"
 # CONFIG_DISPLAY_CPUINFO is not set
 CONFIG_DISPLAY_BOARDINFO_LATE=y
 CONFIG_SPL_MAX_SIZE=0x40000

target/linux/rockchip/armv8/base-files/etc/board.d/02_network
@@ -7,6 +7,7 @@ rockchip_setup_interfaces()
 	local board="$1"
 
 	case "$board" in
+	9tripod,x3568-v4|\
 	ariaboard,photonicat|\
 	armsom,sige7|\
 	ezpro,mrkaio-m68s|\
@@ -19,7 +20,6 @@ rockchip_setup_interfaces()
 	friendlyarm,nanopi-r4se|\
 	friendlyarm,nanopi-r4s-enterprise|\
 	friendlyarm,nanopi-r6c|\
-	ninetripod,x3568-v4|\
 	radxa,rockpi-e|\
 	xunlong,orangepi-r1-plus|\
 	xunlong,orangepi-r1-plus-lts)

target/linux/rockchip/armv8/base-files/etc/hotplug.d/net/40-net-smp-affinity
@@ -29,6 +29,7 @@ set_interface_core() {
 }
 
 case "$(board_name)" in
+9tripod,x3568-v4|\
 ariaboard,photonicat|\
 armsom,sige3|\
 armsom,sige7|\
@@ -38,7 +39,6 @@ friendlyarm,nanopi-r3s|\
 friendlyarm,nanopi-r5c|\
 lunzn,fastrhino-r66s|\
 mmbox,anas3035|\
-ninetripod,x3568-v4|\
 nlnet,xiguapi-v3|\
 radxa,e25|\
 sinovoip,rk3568-bpi-r2pro|\

target/linux/rockchip/armv8/base-files/etc/init.d/phy-leds
@@ -27,6 +27,12 @@ netled_set() {
 
 boot() {
 	case "$(board_name)" in
+	9tripod,x3568-v4)
+		netled_set "stmmac-0:03:amber:lan" "eth1" "link_10 link_100 link_1000" "1" "rx tx" "1"
+		netled_set "stmmac-0:03:green:lan" "eth1" "link_10 link_100 link_1000" "1" "rx tx" "0"
+		netled_set "stmmac-1:02:amber:lan" "eth0" "link_10 link_100 link_1000" "1" "rx tx" "1"
+		netled_set "stmmac-1:02:green:lan" "eth0" "link_10 link_100 link_1000" "1" "rx tx" "0"
+		;;
 	ariaboard,photonicat)
 		netled_set "stmmac-0:03:amber:lan" "eth1" "link_10 link_100 link_1000" "0" "rx tx" "1"
 		netled_set "stmmac-0:03:green:lan" "eth1" "link_10 link_100 link_1000" "1" "rx tx" "0"
@@ -73,12 +79,6 @@ boot() {
 		netled_set "stmmac-0:01:green:lan" "lan1" "link_10 link_100 link_1000" "1" "rx tx" "0"
 		netled_set "stmmac-1:01:amber:lan" "lan2" "link_10 link_100 link_1000" "0" "rx tx" "1"
 		netled_set "stmmac-1:01:green:lan" "lan2" "link_10 link_100 link_1000" "1" "rx tx" "0"
-		;;
-	ninetripod,x3568-v4)
-		netled_set "stmmac-0:03:amber:lan" "eth1" "link_10 link_100 link_1000" "1" "rx tx" "1"
-		netled_set "stmmac-0:03:green:lan" "eth1" "link_10 link_100 link_1000" "1" "rx tx" "0"
-		netled_set "stmmac-1:02:amber:wan" "eth0" "link_10 link_100 link_1000" "1" "rx tx" "1"
-		netled_set "stmmac-1:02:green:wan" "eth0" "link_10 link_100 link_1000" "1" "rx tx" "0"
 		;;
 	radxa,rock3a)
 		netled_set "stmmac-1:01:amber:lan" "eth0" "link_10 link_100 link_1000" "0" "rx tx" "1"

commit 5048a619a8693a9082b78a7672d63d169a951bb5

rockchip: fix path to 9Tripod X3568 v4 dtb
Fixes: 1ab167368939 ("rockchip: backport upstreamed dts for 9Tripod X3568 v4")
Signed-off-by: Tianling Shen <cnsztl@immortalwrt.org>

target/linux/rockchip/image/armv8.mk
@@ -16,7 +16,7 @@ define Device/9tripod_x3568-v4
   DEVICE_MODEL := X3568
   DEVICE_VARIANT := v4
   SOC := rk3568
-  DEVICE_DTS := rk3568-9tripod-x3568-v4
+  DEVICE_DTS := rockchip/rk3568-9tripod-x3568-v4
   BOOT_FLOW := pine64-img
   SUPPORTED_DEVICES := ninetripod,x3568-v4
   DEVICE_PACKAGES := blkdiscard block-mount kmod-ata-ahci-dwc kmod-nvme kmod-hwmon-pwmfan \