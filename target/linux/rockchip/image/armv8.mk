9f9e376a114b1548f6418abbc28b59bfb18e9912
dreak7 <145880595+dreak7@users.noreply.github.com>
2026-05-01 18:51:07 +0800

    Update armv8.mk

--- target/linux/rockchip/image/armv8.mk
+++ target/linux/rockchip/image/armv8.mk
@@ -20,6 +20,15 @@ define Device/rk3399
   BOOT_FLOW := pine64-bin
 endef
 
+define Device/embedfire_doornet2
+  DEVICE_VENDOR := EmbedFire
+  DEVICE_MODEL := DoorNet2
+  SOC := rk3399
+  BOOT_FLOW := pine64-bin
+  DEVICE_PACKAGES := kmod-r8168 -urngd
+endef
+TARGET_DEVICES += embedfire_doornet2
+
 define Device/rk3528
   SOC := rk3528
   KERNEL_LOADADDR := 0x03000000
